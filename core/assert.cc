617f20cf0118f027ea90597a0590857069ac7df0
Gustav <sir.gustav.the.coder@gmail.com>
2017-02-22 08:43:55 +0100

    untested basic assert

--- core/assert.cc
+++ core/assert.cc
@@ -1,2 +1,14 @@
 #include "core/assert.h"
 
+#include <iostream>
+
+
+namespace assertlib {
+
+void OnAssert(const char* const expression, int line, const char* const file, const char* const function) {
+  std::cerr << "Assertion failed: " << expression << "\n"
+            << "File: " << file << " at " << line << "\n"
+            << "Function: " << function << "\n";
+}
+
+}
--- core/assert.h
+++ core/assert.h
@@ -1,6 +1,10 @@
 #ifndef CORE_ASSERT_H
 #define CORE_ASSERT_H
 
-#include <cassert>
+namespace assertlib {
+void OnAssert(const char* const expression, int line, const char* const file, const char* const function);
+}
+
+#define assert(x) do { if(x) {} else { ::assertlib::OnAssert(#x, __LINE__, __FILE__, __FUNCTION__); } } while(false)
 
 #endif  // CORE_ASSERT_H
