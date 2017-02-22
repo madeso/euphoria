70a30541bae57d3a862bd1f78a9478b1023a530a
Gustav <sir.gustav.the.coder@gmail.com>
2017-02-22 21:06:15 +0100

    cleaned up assert

--- core/assert.cc
+++ core/assert.cc
@@ -7,8 +7,10 @@ namespace assertlib {
 
 void OnAssert(const char* const expression, int line, const char* const file, const char* const function) {
   std::cerr << "Assertion failed: " << expression << "\n"
-            << "File: " << file << " at " << line << "\n"
-            << "Function: " << function << "\n";
+            << "Function: " << function << "\n"
+            << "File: " << file << ":" << line << "\n";
+
+  exit(-1);
 }
 
 }
--- core/assert.h
+++ core/assert.h
@@ -1,11 +1,20 @@
 #ifndef CORE_ASSERT_H
 #define CORE_ASSERT_H
 
+#if 0
+
+#include <cassert>
+#define Assert(x) assert(x)
+
+#else
+
 namespace assertlib {
 void OnAssert(const char* const expression, int line, const char* const file, const char* const function);
 }
 
 // todo: stb libraries and rapidjson aren't using our assert
-#define Assert(x) do { if(x) {} else { ::assertlib::OnAssert(#x, __LINE__, __FILE__, __FUNCTION__); } } while(false)
+#define Assert(x) do { if(x) {} else { ::assertlib::OnAssert(#x, __LINE__, __FILE__, __PRETTY_FUNCTION__); } } while(false)
+
+#endif
 
 #endif  // CORE_ASSERT_H
