bcedd0bdc4f09194c9ea07264fe85b9f1692782d
Gustav <sir.gustav.the.coder@gmail.com>
2017-02-25 12:12:12 +0100

    fixed so that the index count was based on the current quad, and not the next quad

--- render/bufferbuilder2d.cc
+++ render/bufferbuilder2d.cc
@@ -26,15 +26,17 @@ void BufferBuilder2d::AddTriangle(unsigned int a, unsigned int b, unsigned int c
 void BufferBuilder2d::AddQuad(const Point& a, const Point& b, const Point& c,
              const Point& d) {
   Assert(this);
-  AddVertex(a);
-  AddVertex(b);
-  AddVertex(c);
-  AddVertex(d);
 
   const unsigned int ai = data.size() / 4;
   const unsigned int bi = ai + 1;
   const unsigned int ci = ai + 2;
   const unsigned int di = ai + 3;
+
+  AddVertex(a);
+  AddVertex(b);
+  AddVertex(c);
+  AddVertex(d);
+  
   AddTriangle(ci, bi, ai);
   AddTriangle(ci, di, bi);
 }
