ae70acdc28d6547b4cca0a69a05bd28b5d467b3f
Gustav <sir.gustav.the.coder@gmail.com>
2017-02-25 11:10:06 +0100

    removed comments

--- render/buffer2d.cc
+++ render/buffer2d.cc
@@ -7,21 +7,16 @@
 Buffer2d::Buffer2d(const BufferBuilder2d& bb)
   : tri_count_(bb.GetTriangleIndices().size()/3)
 {
-  // 1. Bind Vertex Array Object
   Vao::Bind(&vao_);
   Vbo::Bind(&vbo_);
 
-  // 2. Copy our vertices array in a vertex buffer for OpenGL to use
   vbo_.SetData(bb.GetVertexData());
 
-  // 3. Copy our index array in a element buffer for OpenGL to use
   Ebo::Bind(&ebo_);
   ebo_.SetData(bb.GetTriangleIndices());
 
-  // 3. Then set the vertex attributes pointers
   vao_.BindVboData(attributes2d::Vertex(), sizeof(float) * 4, 0);
 
-  // 4. Unbind VAO (NOT the EBO)
   Vao::Bind(nullptr);
 
   Ebo::Bind(nullptr);
