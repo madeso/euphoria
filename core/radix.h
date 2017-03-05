062f90bfd9dc6b8718ce81f1fa7874b7943c63fd
Gustav <sir.gustav.the.coder@gmail.com>
2017-03-05 21:24:11 +0100

    16bit radix sort

--- core/radix.h
+++ core/radix.h
@@ -9,6 +9,41 @@ struct Bucket10base {
   static int GetIndex(Int i, int exp) {
     return (i / exp) % 10;
   }
+
+  static int GetStart() {
+    return 1;
+  }
+
+  static bool KeepLooping(Int max_value, int exp) {
+    return max_value/exp > 0;
+  }
+
+  static int NextValue(Int, int exp) {
+    return exp * 10;
+  }
+};
+
+template <typename Int>
+struct Bucket16base {
+  enum { COUNT = 16 };
+  static int GetIndex(Int i, int exp) {
+    return (i >> (exp*4)) & 0xF;
+  }
+
+  static int GetStart() {
+    return 0;
+  }
+
+  static bool KeepLooping(Int max_value, int exp) {
+    if(exp==0) return true;
+    // -1 because we need to be sure we have passed the max value
+    // todo: use some smart bit technique to set F up to exp
+    return max_value >= (1 << ((exp-1)*4));
+  }
+
+  static int NextValue(Int, int exp) {
+    return exp + 1;
+  }
 };
 
 template<typename T, typename IdExtractor, typename Int>
@@ -60,7 +95,7 @@ void RadixSort(std::vector<T>* arr)
 {
   const Int max_value = GetMax<T, IdExtractor, Int>(*arr);
 
-  for (Int exp = 1; max_value/exp > 0; exp *= 10) {
+  for (int exp = Bucket::GetStart(); Bucket::KeepLooping(max_value, exp); exp = Bucket::NextValue(max_value, exp)) {
     *arr = CountSort<T, IdExtractor, Bucket, Int>(*arr, exp);
   }
 }
