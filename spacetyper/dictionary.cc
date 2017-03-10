58a0afae3cebc96cf4688b89d6ff761406633fcf
Gustav <sir.gustav.the.coder@gmail.com>
2017-03-11 00:47:48 +0100

    started deleting non spacetyper stuff

--- spacetyper/dictionary.cc
+++
@@ -1,8 +0,0 @@
-#include "spacetyper/dictionary.h"
-
-Dictionary::Dictionary() : adjectives_("wordlist_adjective.txt"), nouns_("wordlist_noun.txt") {
-}
-
-std::string Dictionary::Generate() const {
-  return adjectives_.RandomWord() + " " + nouns_.RandomWord();
-}
