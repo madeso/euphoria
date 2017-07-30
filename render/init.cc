45410b12c24c10c95c773f5392ad796279746754
Gustav <sir.gustav.the.coder@gmail.com>
2017-07-30 22:38:14 +0200

    improved version number print

--- render/init.cc
+++ render/init.cc
@@ -5,15 +5,17 @@
 #include <iostream>
 
 Init::Init() : ok(true) {
-  std::cout << "Created OpenGL " << glGetString(GL_VERSION) << " context"
-            << "\n";
-
   const GLenum err = glewInit();
   if (GLEW_OK != err) {
     std::cerr << "Failed to init glew, error: " << glewGetErrorString(err)
               << "\n";
     ok = false;
   }
+
+  std::cout << "Vendor:         " << glGetString(GL_VENDOR) << "\n";
+  std::cout << "Renderer:       " << glGetString(GL_RENDERER) << "\n";
+  std::cout << "Version OpenGL: " << glGetString(GL_VERSION) << "\n";
+  std::cout << "Version GLSL:   " << glGetString(GL_SHADING_LANGUAGE_VERSION) << "\n";
 
   glDisable(GL_DEPTH_TEST);
   glDisable(GL_CULL_FACE);
