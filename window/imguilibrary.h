75eebc696674e12f475b23ecba64fe7b5e1d213c
Gustav <sir.gustav.the.coder@gmail.com>
2017-08-20 20:40:47 +0200

    fixed headerguard

--- window/imguilibrary.h
+++ window/imguilibrary.h
@@ -1,22 +1,26 @@
-#ifndef EUPHORIA_IMGUI_H
-#define EUPHORIA_IMGUI_H
+#ifndef EUPHORIA_IMGUILIBRARY_H
+#define EUPHORIA_IMGUILIBRARY_H
 
 struct SDL_Window;
 typedef union SDL_Event SDL_Event;
 
-class ImguiLibrary {
+class ImguiLibrary
+{
  public:
   explicit ImguiLibrary(SDL_Window* window);
   ~ImguiLibrary();
 
-  void ProcessEvents(SDL_Event*);
+  void
+  ProcessEvents(SDL_Event*);
 
-  void Begin();
-  void Render();
+  void
+  Begin();
+  void
+  Render();
 
  private:
-  SDL_Window *window_;
+  SDL_Window* window_;
 };
 
 
-#endif //EUPHORIA_IMGUI_H
+#endif  // EUPHORIA_IMGUILIBRARY_H
