8811c10b0be95a9dc873d0c68bd5aaf38a8240bf
Gustav <sir.gustav.the.coder@gmail.com>
2017-08-20 10:42:22 +0200

    replaced complicated function with cast

--- render/attributebinder.cc
+++ render/attributebinder.cc
@@ -2,31 +2,10 @@
 
 #include "render/compiledmesh.h"
 
-int
-GetCount(ShaderAttributeSize size)
-{
-  switch(size)
-  {
-    case ShaderAttributeSize::VEC1:
-      return 1;
-    case ShaderAttributeSize::VEC2:
-      return 2;
-    case ShaderAttributeSize::VEC3:
-      return 3;
-    case ShaderAttributeSize::VEC4:
-      return 4;
-    case ShaderAttributeSize::MAT44:
-      return 4 * 4;
-  }
-  DIE("Unhandled case");
-  return 1;
-}
-
 void
 AttributeBinder::Register(const ShaderAttribute& attribute)
 {
-  const int single_size = sizeof(float);
-  const int size        = single_size * GetCount(attribute.size);
+  const int size = sizeof(float) * static_cast<int>(attribute.size);
   bind_datas_.emplace_back(BindData{attribute, size});
   total_size_ += size;
 }
