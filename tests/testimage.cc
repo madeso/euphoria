0e98e6add34e0be704e4d989c5c96d1ab38222a9
Gustav <sir.gustav.the.coder@gmail.com>
2017-08-29 23:38:49 +0200

    added more color tests

--- tests/testimage.cc
+++ tests/testimage.cc
@@ -6,26 +6,50 @@
 
 #include "catch.hpp"
 
-const char* const WHITE_IMAGE =
-    "iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAABmJLR0QA/wD/"
+// 4x4 image without transperency
+// white  / red
+// green / blue
+const char* const TEST_IMAGE =
+    "iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAIAAAD91JpzAAAABmJLR0QA/wD/"
     "AP+"
-    "gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4QgdFRwdHFP2GwAAABVJREFUGNNj/"
-    "P//PwNuwMSAF4xUaQCl4wMR/9A5uQAAAABJRU5ErkJggg==";
+    "gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4QgdFSAdb6CP5AAAABVJREFUCNcFw"
+    "QEBAAAAgBD/TxeqIDQz2gX7fv3PJgAAAABJRU5ErkJggg==";
 
 TEST_CASE("image-load", "[img]")
 {
   FileSystem fs;
   auto       catalog = FileSystemRootCatalog::AddRoot(&fs);
-  catalog->RegisterFileData("white", base64::Decode(WHITE_IMAGE));
+  catalog->RegisterFileData("white", base64::Decode(TEST_IMAGE));
+
+  auto loaded = LoadImage(&fs, "white", AlphaLoad::Remove);
+  REQUIRE(loaded.error == "");
+  REQUIRE_FALSE(loaded.image.HasAlpha());
 
   SECTION("load-white")
   {
-    auto loaded = LoadImage(&fs, "white", AlphaLoad::Remove);
-    REQUIRE(loaded.error == "");
-    REQUIRE_FALSE(loaded.image.HasAlpha());
-
     const auto pixel = loaded.image.GetPixel(0, 0);
     const auto white = Rgba{1.0f, 1.0f, 1.0f, 1.0f};
+    REQUIRE(pixel == approx(white));
+  }
+
+  SECTION("load-red")
+  {
+    const auto pixel = loaded.image.GetPixel(1, 0);
+    const auto white = Rgba{1.0f, 0.0f, 0.0f, 1.0f};
+    REQUIRE(pixel == approx(white));
+  }
+
+  SECTION("load-green")
+  {
+    const auto pixel = loaded.image.GetPixel(0, 1);
+    const auto white = Rgba{0.0f, 1.0f, 0.0f, 1.0f};
+    REQUIRE(pixel == approx(white));
+  }
+
+  SECTION("load-blue")
+  {
+    const auto pixel = loaded.image.GetPixel(1, 1);
+    const auto white = Rgba{0.0f, 0.0f, 1.0f, 1.0f};
     REQUIRE(pixel == approx(white));
   }
 }
