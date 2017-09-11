8a60cf602473656b6ba1cef1664be205d9b955cc
Gustav <sir.gustav.the.coder@gmail.com>
2017-09-11 19:22:26 +0200

    added filesystem wrappers of sdl functions

---
+++ window/filesystem.cc
@@ -0,0 +1,35 @@
+#include "window/filesystem.h"
+
+#include <SDL_filesystem.h>
+
+std::string
+GetBasePath()
+{
+  char* base_path = SDL_GetBasePath();
+  if(base_path)
+  {
+    std::string r = base_path;
+    SDL_free(base_path);
+    return r;
+  }
+  else
+  {
+    return "./";
+  }
+}
+
+std::string
+GetPrefPath()
+{
+  char* base_path = SDL_GetPrefPath("madeso", "euphoria demo");
+  if(base_path)
+  {
+    std::string pref_path = base_path;
+    SDL_free(base_path);
+    return pref_path;
+  }
+  else
+  {
+    return "";
+  }
+}
---
+++ window/filesystem.h
@@ -0,0 +1,13 @@
+#ifndef EUPHORIA_WINDOW_FILESYSTEM_H
+#define EUPHORIA_WINDOW_FILESYSTEM_H
+
+#include <string>
+
+std::string
+GetBasePath();
+
+std::string
+GetPrefPath();
+
+
+#endif  // EUPHORIA_FILESYSTEM_H
