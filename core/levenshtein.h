721f6a3212609457cfd111c864b43763782e5340
Gustav <sir.gustav.the.coder@gmail.com>
2017-12-30 23:46:53 +0100

    added edit distance

---
+++ core/levenshtein.h
@@ -0,0 +1,14 @@
+#ifndef CORE_LEVENSHTEIN_H
+#define CORE_LEVENSHTEIN_H
+
+#include <string>
+
+// Levenshtein Distance Algorithm: C++ Implementation
+// by Anders Sewerin Johansen
+// http://www.merriampark.com/ldcpp.htm
+
+int
+LevenshteinDistance(const std::string& source, const std::string& target);
+
+
+#endif  // CORE_LEVENSHTEIN_H
---
+++ tests/testlevenshtein.cc
@@ -0,0 +1,13 @@
+#include "core/levenshtein.h"
+
+#include "catch.hpp"
+
+TEST_CASE("levenshtein")
+{
+  const std::string a        = "kitten";
+  const std::string b        = "sitting";
+  cons int          distance = 3;
+
+  CHECK(LevenshteinDistance(a, b) == distance);
+  CHECK(LevenshteinDistance(b, a) == distance);
+}
