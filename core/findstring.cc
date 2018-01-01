f722eecc8a8fa1aec42510f939e16613e727d5da
Gustav <sir.gustav.the.coder@gmail.com>
2018-01-01 17:33:15 +0100

    added string fund functions

---
+++ core/findstring.cc
@@ -0,0 +1,51 @@
+#include "core/findstring.h"
+
+bool
+Find(const std::string& target, const std::string& search)
+{
+  return target.find(search) != std::string::npos;
+}
+
+bool
+Find(const std::string& target, const std::vector<std::string>& searches)
+{
+  for(const auto& search : searches)
+  {
+    if(!Find(target, search))
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool
+Find(const std::vector<std::string>& targets, const std::string& search)
+{
+  for(const auto& target : targets)
+  {
+    if(Find(target, search))
+    {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool
+Find(
+    const std::vector<std::string>& targets,
+    const std::vector<std::string>& searches)
+{
+  for(const auto& target : targets)
+  {
+    if(Find(target, searches))
+    {
+      return true;
+    }
+  }
+
+  return false;
+}
---
+++ core/findstring.h
@@ -0,0 +1,27 @@
+#ifndef CORE_FINDSTRING_H
+#define CORE_FINDSTRING_H
+
+#include <vector>
+#include <string>
+
+// find part of a string in another
+// todo: replace with a regex instead?
+bool
+Find(const std::string& target, const std::string& search);
+
+// find all of the strings in the target
+bool
+Find(const std::string& target, const std::vector<std::string>& searches);
+
+// find the string in any target
+bool
+Find(const std::vector<std::string>& targets, const std::string& search);
+
+// find all of the strings in any target
+bool
+Find(
+    const std::vector<std::string>& targets,
+    const std::vector<std::string>& searches);
+
+
+#endif  // CORE_FINDSTRING_H
