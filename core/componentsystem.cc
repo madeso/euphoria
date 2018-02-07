1618f0a0b9a355180dd4c8d7a67c886ca8ff9af2
Gustav <sir.gustav.the.coder@gmail.com>
2018-02-07 21:56:07 +0100

    refactored component system

--- core/componentsystem.cc
+++ core/componentsystem.cc
@@ -1,3 +1,21 @@
 #include "core/componentsystem.h"
 
-// here to test compilation of componentsystem.h
+ComponentSystem::ComponentSystem(const std::string& the_name)
+    : name(the_name)
+{
+}
+
+void
+ComponentSystemStore::Add(std::shared_ptr<ComponentSystem> system)
+{
+  systems.emplace_back(system);
+}
+
+void
+ComponentSystemUpdateStore::Update(float dt) const
+{
+  for(const auto s : systems)
+  {
+    s->Update(dt);
+  }
+}
--- core/componentsystem.h
+++ core/componentsystem.h
@@ -1,29 +1,66 @@
 #ifndef EUPHORIA_COMPONENTSYSTEM_H
 #define EUPHORIA_COMPONENTSYSTEM_H
 
-#include "core/id.h"
-
+#include <memory>
+#include <string>
 #include <vector>
 
+// only a base class container
 class ComponentSystem
 {
  public:
-  ComponentSystem()          = default;
+  explicit ComponentSystem(const std::string& the_name);
   virtual ~ComponentSystem() = default;
 
-  // todo: add creation data
-  virtual void
-  Create(IdGenerator::ID object) = 0;
-
-  virtual bool
-  Has(IdGenerator::ID object) const = 0;
-
-  virtual void
-  Destroy(IdGenerator::ID object) = 0;
-
-  virtual std::vector<IdGenerator::ID>
-  List() const = 0;
+  // for debug purposes
+  const std::string name;
 };
 
+class ComponentSystemStore
+{
+ public:
+  ComponentSystemStore() = default;
+
+  void
+  Add(std::shared_ptr<ComponentSystem> system);
+
+ private:
+  std::vector<std::shared_ptr<ComponentSystem>> systems;
+};
+
+class ComponentSystemUpdate
+{
+ public:
+  virtual void
+  Update(float dt) const = 0;
+};
+
+template <typename TSystem>
+class SystemStore
+{
+ public:
+  void
+  Add(TSystem* system)
+  {
+    systems.emplace_back(system);
+  }
+
+ protected:
+  std::vector<TSystem*> systems;
+};
+
+class ComponentSystemUpdateStore : public SystemStore<ComponentSystemUpdate>
+{
+ public:
+  void
+  Update(float dt) const;
+};
+
+class Systems
+{
+ public:
+  ComponentSystemStore       store;
+  ComponentSystemUpdateStore update;
+};
 
 #endif  // EUPHORIA_COMPONENTSYSTEM_H
