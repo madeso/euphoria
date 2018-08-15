c4ef88a4455c2e4beb9dee4489237c7c5df3ad74
Gustav <sir.gustav.the.coder@gmail.com>
2018-08-15 22:07:33 +0200

    stuff compiles

--- main.cc
+++ main.cc
@@ -1,125 +1,156 @@
-#include <SDL.h>
+#include <SDL2/SDL.h>
 #include "synth.h"
 
 class App
 {
-public:
-    App()
-        : display(0)
-        , playing(false)
+ public:
+  App()
+      : playing(false)
+  {
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
     {
-        if(SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-            throw "Failed to init";
-        }
-
-        if((display = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF)) == NULL) {
-            throw "Failed to set videomode";
-        }
-
-        setupAudio();
+      throw "Failed to init";
     }
 
-    void Draw() 
+    if(SDL_CreateWindowAndRenderer(
+           640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer))
     {
-        if( playing )
-        {
-            FillRect(25,25,100,150,0xFFFFFF);
-        }
+      SDL_LogError(
+          SDL_LOG_CATEGORY_APPLICATION,
+          "Couldn't create window and renderer: %s",
+          SDL_GetError());
+      throw "Failed to set videomode";
     }
 
-    void OnRender() 
+    setupAudio();
+  }
+
+  ~App()
+  {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+  };
+
+  void
+  Draw()
+  {
+    if(playing)
     {
-        FillRect(0,0,640,480,0x000000);
-        Draw();
-        SDL_Flip(display);
+      FillRect(25, 25, 100, 150, 0xFFFFFF);
     }
+  }
 
-    void OnKey( SDLKey sym, SDLMod mod, bool down ) 
+  void
+  OnRender()
+  {
+    SDL_RenderClear(renderer);
+    Draw();
+    SDL_RenderPresent(renderer);
+  }
+
+  void
+  OnKey(SDL_Keycode, Uint16, bool down)
+  {
+    playing = down;
+  }
+
+  void
+  Audiocallback(Uint8* stream, int len)
+  {
+    if(playing)
     {
-        playing = down;
+      unsigned int l    = len / 2;
+      float*       fbuf = new float[l];
+      memset(fbuf, 0, sizeof(fbuf));
+      synth.SynthSample(l, fbuf);
+      while(l--)
+      {
+        float f = fbuf[l];
+        if(f < -1.0)
+          f = -1.0;
+        if(f > 1.0)
+          f                  = 1.0;
+        ((Sint16*)stream)[l] = (Sint16)(f * 32767);
+      }
+      delete[] fbuf;
     }
+    else
+      memset(stream, 0, len);
+  }
 
-    void Audiocallback(Uint8 *stream, int len)
-    {
-        if (playing)
-        {
-            unsigned int l = len/2;
-            float* fbuf = new float[l];
-            memset(fbuf, 0, sizeof(fbuf));
-            synth.SynthSample(l, fbuf);
-            while (l--)
-            {
-                float f = fbuf[l];
-                if (f < -1.0) f = -1.0;
-                if (f > 1.0) f = 1.0;
-                ((Sint16*)stream)[l] = (Sint16)(f * 32767);
-            }
-            delete[] fbuf;
-        }
-        else memset(stream, 0, len);
-    }
+ private:
+  void
+  FillRect(int x, int y, int w, int h, int color)
+  {
+    // SDL_Rect rect = {x, y, w, h};
+    // SDL_FillRect(display, &rect, color);
+  }
 
-private:
-    void FillRect(int x, int y, int w, int h, int color)
-    {
-        SDL_Rect rect = {x,y,w,h};
-        SDL_FillRect(display, &rect, color);
-    }
+  void
+  setupAudio();
 
-    void setupAudio();
+ private:
+  Synth synth;
+  bool  playing;
 
-private:
-    SDL_Surface* display;
-    Synth synth;
-    bool playing;
+  SDL_Window*   window;
+  SDL_Renderer* renderer;
 };
 
-static void SDLAudioCallback(void *userdata, Uint8 *stream, int len)
+static void
+SDLAudioCallback(void* userdata, Uint8* stream, int len)
 {
-    App* app = (App*) userdata;
-    app->Audiocallback(stream, len);
+  App* app = (App*)userdata;
+  app->Audiocallback(stream, len);
 }
 
-void App::setupAudio()
+void
+App::setupAudio()
 {
-    SDL_AudioSpec des;
-    des.freq = 44100;
-    des.format = AUDIO_S16SYS;
-    des.channels = 1;
-    des.samples = 512;
-    des.callback = SDLAudioCallback;
-    des.userdata = this;
-    if( 0 != SDL_OpenAudio(&des, NULL) ) throw "Failed to start audio";
-    SDL_PauseAudio(0);
+  SDL_AudioSpec des;
+  des.freq     = 44100;
+  des.format   = AUDIO_S16SYS;
+  des.channels = 1;
+  des.samples  = 512;
+  des.callback = SDLAudioCallback;
+  des.userdata = this;
+  if(0 != SDL_OpenAudio(&des, nullptr))
+    throw "Failed to start audio";
+  SDL_PauseAudio(0);
 }
 
-int main(int argc, char* argv[])
+int
+main(int argc, char* argv[])
 {
-    App app;
-    SDL_Event Event;
+  App       app;
+  SDL_Event event;
 
-    bool run = true;
-    while(run)
+  bool run = true;
+  while(run)
+  {
+    while(SDL_PollEvent(&event))
     {
-        while(SDL_PollEvent(&Event))
-        {
-            switch(Event.type)
-            {
-                case SDL_QUIT:
-                    run = false;
-                break;
-                case SDL_KEYDOWN:
-                case SDL_KEYUP:
-                    app.OnKey(Event.key.keysym.sym, Event.key.keysym.mod, Event.type == SDL_KEYDOWN);
-                    break;
-            }
-        }
-
-        //OnLoop();
-        app.OnRender();
+      switch(event.type)
+      {
+        case SDL_QUIT:
+          run = false;
+          break;
+        case SDL_KEYDOWN:
+        case SDL_KEYUP:
+          app.OnKey(
+              event.key.keysym.sym,
+              event.key.keysym.mod,
+              event.type == SDL_KEYDOWN);
+          break;
+        default:
+          break;
+      }
     }
 
-      SDL_Quit();
+    app.OnRender();
+  }
 
-    return 1;
+  SDL_Quit();
+
+  return 1;
 }
