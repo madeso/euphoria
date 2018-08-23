c46c5b6b8f58fe66b08288d26cf2eb59caa6a20f
Gustav <sir.gustav.the.coder@gmail.com>
2018-08-23 18:54:00 +0200

    documented knob code

--- imgui_extra.cc
+++ imgui_extra.cc
@@ -1,6 +1,8 @@
 #include "imgui_extra.h"
 
 #include "imgui.h"
+
+#define _USE_MATH_DEFINES
 #include <cmath>
 
 namespace imgui
@@ -8,44 +10,56 @@ namespace imgui
   bool
   Knob(const char* label, float* p_value, float v_min, float v_max)
   {
+    constexpr auto  pi           = static_cast<float>(M_PI);
+    constexpr float angle_min    = pi * 0.75f;
+    constexpr float angle_max    = pi * 2.25f;
+    constexpr float radius_outer = 20.0f;
+    constexpr float radius_inner = radius_outer * 0.40f;
+
     ImGuiIO&    io    = ImGui::GetIO();
     ImGuiStyle& style = ImGui::GetStyle();
 
-    float       radius_outer = 20.0f;
-    ImVec2      pos          = ImGui::GetCursorScreenPos();
+    ImVec2      pos    = ImGui::GetCursorScreenPos();
     ImVec2      center = ImVec2(pos.x + radius_outer, pos.y + radius_outer);
     float       line_height = ImGui::GetTextLineHeight();
     ImDrawList* draw_list   = ImGui::GetWindowDrawList();
-
-    float ANGLE_MIN = 3.141592f * 0.75f;
-    float ANGLE_MAX = 3.141592f * 2.25f;
 
     ImGui::InvisibleButton(
         label,
         ImVec2(
             radius_outer * 2,
             radius_outer * 2 + line_height + style.ItemInnerSpacing.y));
-    bool value_changed = false;
-    bool is_active     = ImGui::IsItemActive();
-    bool is_hovered    = ImGui::IsItemHovered();
-    if(is_active && io.MouseDelta.x != 0.0f)
+    const bool is_active  = ImGui::IsItemActive();
+    const bool is_hovered = ImGui::IsItemHovered();
+
+    // changing value
+    const bool value_changed = is_active && io.MouseDelta.x != 0.0f;
+    if(value_changed)
     {
       float step = (v_max - v_min) / 200.0f;
       *p_value += io.MouseDelta.x * step;
       if(*p_value < v_min)
         *p_value = v_min;
       if(*p_value > v_max)
-        *p_value    = v_max;
-      value_changed = true;
+        *p_value = v_max;
     }
 
-    float t         = (*p_value - v_min) / (v_max - v_min);
-    float angle     = ANGLE_MIN + (ANGLE_MAX - ANGLE_MIN) * t;
-    float angle_cos = cosf(angle), angle_sin = sinf(angle);
-    float radius_inner = radius_outer * 0.40f;
+    const float t         = (*p_value - v_min) / (v_max - v_min);
+    const float angle     = angle_min + (angle_max - angle_min) * t;
+    const float angle_cos = cosf(angle);
+    const float angle_sin = sinf(angle);
 
-    draw_list->AddCircleFilled(
-        center, radius_outer, ImGui::GetColorU32(ImGuiCol_FrameBg), 16);
+    // colors
+    const auto label_color        = ImGui::GetColorU32(ImGuiCol_Text);
+    const auto outer_circle_color = ImGui::GetColorU32(ImGuiCol_FrameBg);
+    const auto value_indicator_color =
+        ImGui::GetColorU32(ImGuiCol_SliderGrabActive);
+    const auto inner_circle_color = ImGui::GetColorU32(
+        is_active ? ImGuiCol_FrameBgActive
+                  : is_hovered ? ImGuiCol_FrameBgHovered : ImGuiCol_FrameBg);
+
+    // visualization
+    draw_list->AddCircleFilled(center, radius_outer, outer_circle_color, 16);
     draw_list->AddLine(
         ImVec2(
             center.x + angle_cos * radius_inner,
@@ -53,20 +67,16 @@ namespace imgui
         ImVec2(
             center.x + angle_cos * (radius_outer - 2),
             center.y + angle_sin * (radius_outer - 2)),
-        ImGui::GetColorU32(ImGuiCol_SliderGrabActive),
+        value_indicator_color,
         2.0f);
-    draw_list->AddCircleFilled(
-        center,
-        radius_inner,
-        ImGui::GetColorU32(
-            is_active
-                ? ImGuiCol_FrameBgActive
-                : is_hovered ? ImGuiCol_FrameBgHovered : ImGuiCol_FrameBg),
-        16);
+    draw_list->AddCircleFilled(center, radius_inner, inner_circle_color, 16);
     draw_list->AddText(
         ImVec2(pos.x, pos.y + radius_outer * 2 + style.ItemInnerSpacing.y),
-        ImGui::GetColorU32(ImGuiCol_Text),
+        label_color,
         label);
+
+
+    // tooltip
     if(is_active || is_hovered)
     {
       ImGui::SetNextWindowPos(ImVec2(
