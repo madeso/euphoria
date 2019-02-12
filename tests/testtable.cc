b33acd6be3b6233bf37e2f6d6687b16e03bde5b0
Gustav <sir.gustav.the.coder@gmail.com>
2019-02-12 06:18:18 +0100

    strings test

--- tests/testtable.cc
+++ tests/testtable.cc
@@ -2,9 +2,10 @@
 
 #include "catch.hpp"
 
-TEST_CASE("table-from_csv", "[table]")
+TEST_CASE("table-from_csv_default", "[table]")
 {
   const auto firstcol = Column{"a", "1"};
+  const auto firstcolstring = Column{"a b", "1"};
   const auto secondcol = Column{"b", "2"};
 
   SECTION("not ending with newline")
@@ -20,6 +21,14 @@ TEST_CASE("table-from_csv", "[table]")
     const auto table = TableFromCsv("a,b\n\n\n1,2\n\n");
     REQUIRE(table.size()==2);
     CHECK(table[0] == firstcol);
+    CHECK(table[1] == secondcol);
+  }
+
+  SECTION("strings")
+  {
+    const auto table = TableFromCsv("\"a b\",b\n1,2");
+    REQUIRE(table.size()==2);
+    CHECK(table[0] == firstcolstring);
     CHECK(table[1] == secondcol);
   }
 }
