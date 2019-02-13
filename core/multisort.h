7c74d3870d301edd18811d94cf0dd2e3c1d43b5e
Gustav <sir.gustav.the.coder@gmail.com>
2019-02-13 20:34:32 +0100

    working on multisort

---
+++ core/multisort.h
@@ -0,0 +1,102 @@
+#ifndef CORE_MULTISORT_H
+#define CORE_MULTISORT_H
+
+#include <vector>
+#include <string>
+#include <numeric>
+#include <algorithm>
+#include <memory>
+
+enum class SortStyle
+{
+  Ascending,
+  Descending
+};
+
+template <typename T>
+struct Sortable
+{
+  virtual ~Sortable() {}
+  virtual int
+  Sort(const T& lhs, const T& rhs) = 0;
+};
+
+template <typename T>
+using SortableList = std::vector<std::shared_ptr<Sortable<T>>>;
+
+template <typename T, typename Value, typename SortFunc>
+struct SortAction : public Sortable<T>
+{
+  Value T::*member;
+  SortStyle sort_style;
+  SortFunc  sort_func;
+
+  SortAction(Value T::*m, SortStyle s, SortFunc f)
+      : member(m)
+      , sort_style(s)
+      , sort_func(f)
+  {
+  }
+
+  int
+  Sort(const T& lhs, const T& rhs) override
+  {
+    const int result = sort_func(lhs.*member, rhs.*member);
+    return sort_style == SortStyle::Ascending ? result : -result;
+  }
+};
+
+template <typename T>
+int
+DefaultSortFunc(T lhs, T rhs)
+{
+  if(lhs == rhs)
+    return 0;
+  return lhs < rhs ? 1 : -1;
+}
+
+template <typename T>
+struct SortBuilder
+{
+  SortableList<T> order;
+
+  template <typename SortFunc, typename Value>
+  SortBuilder<T>&
+  Add(Value T::*member,
+      SortFunc  sort_func,
+      SortStyle sort_style = SortStyle::Ascending)
+  {
+    auto o = std::make_shared<SortAction<T, Value, SortFunc>>(
+        member, sort_style, sort_func);
+    order.emplace_back(o);
+    return *this;
+  }
+
+  template <typename Value>
+  SortBuilder<T>&
+  Add(Value T::*member, SortStyle sort_style = SortStyle::Ascending)
+  {
+    return Add(member, &DefaultSortFunc<Value>, sort_style);
+  }
+};
+
+template <typename T>
+std::vector<size_t>
+GetSortedIndices(const std::vector<T>& data, const SortableList<T>& sort_order)
+{
+  std::vector<size_t> r(data.size());
+  std::iota(std::begin(r), std::end(r), 0);
+  // todo: std::sort doesn't seem to work
+  std::sort(r.begin(), r.end(), [&data, &sort_order](size_t lhs, size_t rhs) -> bool {
+    for(const auto& so : sort_order)
+    {
+      const auto result = so->Sort(data[lhs], data[rhs]);
+      if(result == 1)
+        return true;
+    }
+    return false;
+  });
+  return r;
+}
+
+#endif  // CORE_MULTISORT_H
---
+++ tests/testmultisort.cc
@@ -0,0 +1,63 @@
+#include "core/multisort.h"
+
+#include "catch.hpp"
+
+struct Data
+{
+  int i;
+  int j;
+};
+
+TEST_CASE("multisort-verify", "[multisort]")
+{
+  auto o = SortBuilder<Data>{}.Add(&Data::i, SortStyle::Ascending).order;
+
+  const auto same    = o[0]->Sort(Data{0}, Data{0});
+  const auto less    = o[0]->Sort(Data{1}, Data{0});
+  const auto greater = o[0]->Sort(Data{0}, Data{1});
+
+  CHECK(same == 0);
+  CHECK(less == -1);
+  CHECK(greater == 1);
+}
+
+TEST_CASE("multisort-test", "[multisort]")
+{
+  // example usage
+
+  const std::vector<Data> data{
+      {1, 1},
+      {3, 2},
+      {2, 2},
+  };
+
+  SECTION("basic sort asc")
+  {
+    auto si = GetSortedIndices(
+        data, SortBuilder<Data>{}.Add(&Data::i, SortStyle::Ascending).order);
+    const auto sorted = std::vector<size_t>{0, 2, 1};
+    CHECK(si == sorted);
+  }
+
+  SECTION("basic sort desc")
+  {
+    auto si = GetSortedIndices(
+        data, SortBuilder<Data>{}.Add(&Data::i, SortStyle::Descending).order);
+    const auto sorted = std::vector<size_t>{1, 2, 0};
+    CHECK(si == sorted);
+  }
+
+  /* doesnt work yet :(
+  SECTION("handle second")
+  {
+    auto si = GetSortedIndices(
+        data,
+        SortBuilder<Data>{}
+            .Add(&Data::j, SortStyle::Ascending)
+            .Add(&Data::i, SortStyle::Descending)
+            .order);
+    const auto sorted = std::vector<size_t>{0, 1, 2};
+    CHECK(si == sorted);
+  }
+  */
+}
