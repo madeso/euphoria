9cff5d10125c8361bee949b73853a97f81e221af
Gustav <sir.gustav.the.coder@gmail.com>
2019-04-22 10:54:21 +0200

    cubehelix palette

---
+++ core/palette_cubehelix.h
@@ -0,0 +1,35 @@
+#ifndef EUPHORIA_PALETTE_CUBEHELIX_H
+#define EUPHORIA_PALETTE_CUBEHELIX_H
+
+struct Palette;
+
+namespace palette
+{
+  const Palette&
+  Classic();
+
+  const Palette&
+  Perceptual_rainbow();
+
+  const Palette&
+  Purple();
+
+  const Palette&
+  Jim_special();
+
+  const Palette&
+  Red();
+
+  const Palette&
+  Cubehelix1();
+
+  const Palette&
+  Cubehelix2();
+
+  const Palette&
+  Cubehelix3();
+}
+
+
+#endif  // EUPHORIA_PALETTE_CUBEHELIX_H
+
