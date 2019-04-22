cbf58f6ef8cfd38dc3f34c8f23000d029b64e90b
Gustav <sir.gustav.the.coder@gmail.com>
2019-04-22 19:33:50 +0200

    mpl color palette

---
+++ core/palette_matplot.h
@@ -0,0 +1,17 @@
+#ifndef EUPHORIA_PALETTE_MATPLOT_H
+#define EUPHORIA_PALETTE_MATPLOT_H
+
+struct Palette;
+
+// https://bids.github.io/colormap/
+
+namespace palette
+{
+  const Palette& Magma();
+  const Palette& Inferno();
+  const Palette& Plasma();
+  const Palette& Viridis();
+}
+
+#endif  // EUPHORIA_PALETTE_MATPLOT_H
+
