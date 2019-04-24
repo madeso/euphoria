676101d8f9b079a24de97606b4900daf1f9f1110
Gustav <sir.gustav.the.coder@gmail.com>
2019-04-24 18:41:51 +0200

    added enum to string test

---
+++ tests/testenumtostring.cc
@@ -0,0 +1,66 @@
+#include "core/enumtostring.h"
+
+#include "catch.hpp"
+
+namespace
+{
+  enum class MyEnum
+  {
+    X, Cat, ReallyLongValue
+  };
+}
+
+BEGIN_ENUM_LIST(MyEnum)
+  ENUM_VALUE(MyEnum, X)
+  ENUM_VALUE(MyEnum, Cat)
+  ENUM_VALUE(MyEnum, ReallyLongValue)
+END_ENUM_LIST()
+
+#define CATCH_IS_VECTOR(TYPE, ...) Catch::Matchers::Equals(std::vector<TYPE>{__VA_ARGS__} )
+
+TEST_CASE("enum2string", "[enum2string]")
+{
+  SECTION("single arg ToString")
+  {
+    CHECK(EnumToString(MyEnum::X) == "X");
+    CHECK(EnumToString(MyEnum::Cat) == "Cat");
+    CHECK(EnumToString(MyEnum::ReallyLongValue) == "ReallyLongValue");
+  }
+
+  SECTION("operator<<")
+  {
+    const auto e = MyEnum::Cat;
+    
+    std::stringstream ss;
+    ss << e;
+    
+    CHECK(ss.str() == "Cat");
+  }
+
+  SECTION("no arg ToString")
+  {
+    CHECK_THAT(EnumToString<MyEnum>(), CATCH_IS_VECTOR(std::string, "X", "Cat", "ReallyLongValue"));
+  }
+
+  SECTION("vector arg ToString")
+  {
+    const auto vec = std::vector<MyEnum>{ MyEnum::Cat, MyEnum::X, MyEnum::Cat };
+    CHECK_THAT(EnumToString<MyEnum>(vec), CATCH_IS_VECTOR(std::string, "Cat", "X", "Cat"));
+  }
+
+  SECTION("match different cases")
+  {
+    auto name = GENERATE(Catch::Generators::as<std::string>{}, "cat", "CAT", "Cat", "cAt");
+    const auto match = StringToEnum<MyEnum>(name);
+    CHECK(match.single_match);
+    CHECK_THAT(match.values, CATCH_IS_VECTOR(MyEnum, MyEnum::Cat));
+  }
+
+  SECTION("match suggestions")
+  {
+    const auto match = StringToEnum<MyEnum>("y", 2);
+    CHECK_FALSE(match.single_match);
+    CHECK_THAT(match.values, CATCH_IS_VECTOR(MyEnum, MyEnum::X, MyEnum::Cat));
+  }
+}
+
