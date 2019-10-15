ec501ae43254115a93ac50b37c688fc1606a3bfb
Gustav <sir.gustav.the.coder@gmail.com>
2019-10-15 06:55:02 +0200

    "fixed" tests

--- tests/testminsynth.cc
+++ tests/testminsynth.cc
@@ -11,9 +11,9 @@ TEST_CASE("synth-verify", "[synth]")
     CHECK(synth::ToneToFrequency(0, synth::base_frequencies::a4)
           == Approx(synth::base_frequencies::a4));
     CHECK(synth::ToneToFrequency(-9, synth::base_frequencies::a4)
-          == Approx(261.63f));
+          == Approx(261.63f).margin(0.1));
     CHECK(synth::ToneToFrequency(-12, synth::base_frequencies::a4)
           == Approx(220.0f));
     CHECK(synth::ToneToFrequency(-12 - 9, synth::base_frequencies::a4)
-          == Approx(130.81f));
+          == Approx(130.81f).margin(0.1));
 }
