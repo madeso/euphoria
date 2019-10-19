0508685bce337f582da842a15310344d71530161
Gustav <sir.gustav.the.coder@gmail.com>
2019-10-19 14:17:11 +0200

    all file strings are now raw string literals

--- render/defaultfiles.cc
+++ render/defaultfiles.cc
@@ -9,7 +9,7 @@ namespace euphoria::render
     {
         catalog->RegisterFileString(
                 "default_shader.json",
-                R"(
+                R"json(
                 {
                     "has_light": true,
                     "ambient": "uMaterial.ambient",
@@ -38,158 +38,171 @@ namespace euphoria::render
                             "path": "img-plain/white"
                         }
                     ]
-                })");
+                })json");
         catalog->RegisterFileString(
                 "default_shader.vert",
-                "#version 330 core\n"
-                "in vec3 aPosition;\n"
-                "in vec3 aNormal;\n"
-                "in vec2 aTexCoord;\n"
-                "\n"
-                "out vec2 texCoord;\n"
-                "out vec3 normal;\n"
-                "out vec3 fragPositionWorld;\n"
-                "\n"
-                "uniform mat4 uProjection;\n"
-                "uniform mat4 uView;\n"
-                "uniform mat4 uModel;\n"
-                "uniform mat3 uNormalMatrix;\n"
-                "\n"
-                "void main()\n"
-                "{\n"
-                "    gl_Position = uProjection * uView * uModel * vec4(aPosition, 1.0);\n"
-                "    fragPositionWorld = vec3(uModel * vec4(aPosition, 1.0));\n"
-                "    texCoord = aTexCoord;\n"
-                "    normal = uNormalMatrix * aNormal;\n"
-                "}\n");
+                R"glsl(
+                #version 330 core
+                in vec3 aPosition;
+                in vec3 aNormal;
+                in vec2 aTexCoord;
+                
+                out vec2 texCoord;
+                out vec3 normal;
+                out vec3 fragPositionWorld;
+                
+                uniform mat4 uProjection;
+                uniform mat4 uView;
+                uniform mat4 uModel;
+                uniform mat3 uNormalMatrix;
+                
+                void main()
+                {
+                    gl_Position = uProjection * uView * uModel * vec4(aPosition, 1.0);
+                    fragPositionWorld = vec3(uModel * vec4(aPosition, 1.0));
+                    texCoord = aTexCoord;
+                    normal = uNormalMatrix * aNormal;
+                }
+                )glsl");
         catalog->RegisterFileString(
                 "default_shader.frag",
-                "#version 330 core\n"
-                "struct Material\n"
-                "{\n"
-                "  vec3 ambient;\n"
-                "  vec3 diffuse;\n"
-                "  vec3 specular;\n"
-                "  float shininess;\n"
-                "};\n"
-                "\n"
-                "struct Light\n"
-                "{\n"
-                "  int type;\n"
-                "  vec3 ambient;\n"
-                "  vec3 diffuse;\n"
-                "  vec3 specular;\n"
-                "  \n"
-                "  float attConst;\n"
-                "  float attLin;\n"
-                "  float attQuad;\n"
-                "  float cosCutoffAngleInner;\n"
-                "  float cosCutoffAngleOuter;\n"
-                "  \n"
-                "  vec3 position;\n"
-                "  vec3 direction;\n"
-                "};\n"
-                "\n"
-                "out vec4 FragColor;\n"
-                "\n"
-                "in vec2 texCoord;\n"
-                "in vec3 normal;\n"
-                "in vec3 fragPositionWorld;\n"
-                "\n"
-                "uniform sampler2D uDiffuseMap;\n"
-                "uniform sampler2D uSpecularMap;\n"
-                "\n"
-                "uniform Light uLight;\n"
-                "uniform vec3 uViewPosition;\n"
-                "uniform Material uMaterial;\n"
-                "\n"
-                "void main()\n"
-                "{\n"
-                "    vec3 ambient = uMaterial.ambient * uLight.ambient;\n"
-                "    \n"
-                "    vec3 norm = normalize(normal);\n"
-                "    vec3 lightDir = uLight.type==0 ? normalize(-uLight.direction) :\n"
-                "                    normalize(uLight.position - fragPositionWorld);\n"
-                "    float diffuse_factor = max(0.0, dot(norm, lightDir));\n"
-                "    vec3 diffuse = diffuse_factor * uMaterial.diffuse * \n"
-                "                     uLight.diffuse;\n"
-                "    \n"
-                "    float specularStrength = 0.5;\n"
-                "    vec3 viewDir = normalize(uViewPosition - fragPositionWorld);\n"
-                "    vec3 reflectDir = reflect(-lightDir, norm);\n"
-                "    float spec = pow(max(0, dot(viewDir, reflectDir)), \n"
-                "                   uMaterial.shininess);\n"
-                "    vec3 specular = specularStrength * uMaterial.specular * spec * \n"
-                "                      uLight.specular;\n"
-                "    \n"
-                "    if(uLight.type == 2) {\n"
-                "      float theta = dot(lightDir, normalize(-uLight.direction));\n"
-                "      if(theta > uLight.cosCutoffAngleOuter) {\n"
-                "        float epsilon = uLight.cosCutoffAngleInner - "
-                "                        uLight.cosCutoffAngleOuter;\n"
-                "        float intensity = clamp((theta - uLight.cosCutoffAngleOuter)\n"
-                "                                / epsilon, 0.0, 1.0); \n"
-                "        diffuse  *= intensity;\n"
-                "        specular *= intensity;\n"
-                "      }\n"
-                "      else {\n"
-                "        // outside of spotlight\n"
-                "        diffuse = vec3(0,0,0);\n"
-                "        specular = vec3(0,0,0);\n"
-                "      }\n"
-                "    }\n"
-                "    \n"
-                "    \n"
-                "    if(uLight.type != 0) {\n"
-                "      float dist = length(uLight.position - fragPositionWorld);\n"
-                "      float att = 1.0 / (uLight.attConst + dist*uLight.attLin + \n"
-                "                         dist*dist*uLight.attQuad);\n"
-                "      ambient *= att;\n"
-                "      diffuse *= att;\n"
-                "      specular *= att;\n"
-                "    }\n"
-                "    \n"
-                "    vec3 object_color = texture(uDiffuseMap, texCoord).rgb;\n"
-                "    vec3 specular_map = texture(uSpecularMap, texCoord).rgb;\n"
-                "    \n"
-                "    vec3 result = (ambient + diffuse) * object_color + \n"
-                "                    specular * specular_map;\n"
-                "    \n"
-                "    FragColor = vec4(result, 1.0);\n"
-                "}\n");
-
+                R"glsl(
+                    #version 330 core
+                    struct Material
+                    {
+                        vec3 ambient;
+                        vec3 diffuse;
+                        vec3 specular;
+                        float shininess;
+                    };
+                    
+                    struct Light
+                    {
+                        int type;
+                        vec3 ambient;
+                        vec3 diffuse;
+                        vec3 specular;
+                        
+                        float attConst;
+                        float attLin;
+                        float attQuad;
+                        float cosCutoffAngleInner;
+                        float cosCutoffAngleOuter;
+                        
+                        vec3 position;
+                        vec3 direction;
+                    };
+                    
+                    out vec4 FragColor;
+                    
+                    in vec2 texCoord;
+                    in vec3 normal;
+                    in vec3 fragPositionWorld;
+                    
+                    uniform sampler2D uDiffuseMap;
+                    uniform sampler2D uSpecularMap;
+                    
+                    uniform Light uLight;
+                    uniform vec3 uViewPosition;
+                    uniform Material uMaterial;
+                    
+                    void main()
+                    {
+                        vec3 ambient = uMaterial.ambient * uLight.ambient;
+                        
+                        vec3 norm = normalize(normal);
+                        vec3 lightDir = uLight.type==0 ? normalize(-uLight.direction) : normalize(uLight.position - fragPositionWorld);
+                        float diffuse_factor = max(0.0, dot(norm, lightDir));
+                        vec3 diffuse = diffuse_factor * uMaterial.diffuse * uLight.diffuse;
+                        
+                        float specularStrength = 0.5;
+                        vec3 viewDir = normalize(uViewPosition - fragPositionWorld);
+                        vec3 reflectDir = reflect(-lightDir, norm);
+                        float spec = pow(max(0, dot(viewDir, reflectDir)), uMaterial.shininess);
+                        vec3 specular = specularStrength * uMaterial.specular * spec * uLight.specular;
+                        
+                        if(uLight.type == 2)
+                        {
+                            float theta = dot(lightDir, normalize(-uLight.direction));
+                            if(theta > uLight.cosCutoffAngleOuter)
+                            {
+                                float epsilon = uLight.cosCutoffAngleInner - uLight.cosCutoffAngleOuter;
+                                float intensity = clamp((theta - uLight.cosCutoffAngleOuter) / epsilon, 0.0, 1.0); 
+                                diffuse  *= intensity;
+                                specular *= intensity;
+                            }
+                            else
+                            {
+                                // outside of spotlight
+                                diffuse = vec3(0,0,0);
+                                specular = vec3(0,0,0);
+                            }
+                        }
+                        
+                        if(uLight.type != 0)
+                        {
+                            float dist = length(uLight.position - fragPositionWorld);
+                            float att = 1.0 / (uLight.attConst + dist*uLight.attLin + dist*dist*uLight.attQuad);
+                            ambient *= att;
+                            diffuse *= att;
+                            specular *= att;
+                        }
+                        
+                        vec3 object_color = texture(uDiffuseMap, texCoord).rgb;
+                        vec3 specular_map = texture(uSpecularMap, texCoord).rgb;
+                        
+                        vec3 result = (ambient + diffuse) * object_color + specular * specular_map;
+                        
+                        FragColor = vec4(result, 1.0);
+                    })glsl");
         catalog->RegisterFileString(
                 "basic_shader.json",
-                R"(  {"diffuse": "uDiffuse", "has_light": false, "textures": [], "default_textures": []}  )");
+                R"json(
+                    {
+                        "diffuse": "uDiffuse",
+                        "has_light": false,
+                        "textures": [],
+                        "default_textures": []
+                    }
+                    )json");
         catalog->RegisterFileString(
                 "basic_shader.vert",
-                "#version 330 core\n"
-                "in vec3 aPosition;\n"
-                "\n"
-                "\n"
-                "uniform mat4 uProjection;\n"
-                "uniform mat4 uView;\n"
-                "uniform mat4 uModel;\n"
-                "\n"
-                "void main()\n"
-                "{\n"
-                "    gl_Position = uProjection * uView * uModel * vec4(aPosition, 1.0);\n"
-                "}\n");
+                R"glsl(
+                    #version 330 core
+                    in vec3 aPosition;
+                    
+                    uniform mat4 uProjection;
+                    uniform mat4 uView;
+                    uniform mat4 uModel;
+                    
+                    void main()
+                    {
+                        gl_Position = uProjection * uView * uModel * vec4(aPosition, 1.0);
+                    }
+                    )glsl");
         catalog->RegisterFileString(
                 "basic_shader.frag",
-                "#version 330 core\n"
-                "\n"
-                "uniform vec3 uDiffuse;\n"
-                "\n"
-                "out vec4 FragColor;\n"
-                "\n"
-                "void main()\n"
-                "{\n"
-                "    FragColor = vec4(uDiffuse, 1.0);\n"
-                "}\n");
-
+                R"glsl(#version 330 core
+                
+                uniform vec3 uDiffuse;
+                
+                out vec4 FragColor;
+                
+                void main()
+                {
+                    FragColor = vec4(uDiffuse, 1.0);
+                }
+                )glsl");
         catalog->RegisterFileString(
-                "texture_types.json", R"({"name" : ["Diffuse", "Specular"]})");
+                "texture_types.json", R"json(
+                    {
+                        "name":
+                        [
+                            "Diffuse",
+                            "Specular"
+                        ]
+                    }
+                    )json");
     }
 
 }  // namespace euphoria::render
