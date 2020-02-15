f614fc542fcbaa46a53ca888b5598fd2c20b1129
Gustav <sir.gustav.the.coder@gmail.com>
2020-02-15 17:34:18 +0100

    raytracer step 3: "camera" and sky colors

--- core/raytracer.cc
+++ core/raytracer.cc
@@ -1,23 +1,40 @@
 #include "core/raytracer.h"
 
 #include "core/image.h"
+#include "core/vec3.h"
+#include "core/ray.h"
 
 namespace euphoria::core
 {
+    Rgb
+    GetColor(const UnitRay3f& ray)
+    {
+        const auto t = (ray.dir.y+1)/2.0f;
+        return RgbTransform::Transform
+        (
+            Rgb(1.0f, 1.0f, 1.0f),
+            t,
+            Rgb(0.5f, 0.7f, 1.0f)
+        );
+    }
+
     void
     Raytrace(Image* aimage)
     {
         Image& img = *aimage;
 
+        const auto lower_left_corner = vec3f{-2.0f, -1.0f, -1.0f};
+        const auto horizontal = vec3f{4.0f, 0.0f, 0.0f};
+        const auto vertical = vec3f{0.0f, 2.0f, 0.0f};
+        const auto origin = vec3f{0.0f, 0.0f, 0.0f};
+
         for(int y=0; y<img.GetHeight(); y+=1)
         for(int x=0; x<img.GetWidth(); x+=1)
         {
-            auto color = Rgb
-            {
-                x / static_cast<float>(img.GetWidth()),
-                y / static_cast<float>(img.GetHeight()),
-                0.0f
-            };
+            const auto u = x / static_cast<float>(img.GetWidth());
+            const auto v = y / static_cast<float>(img.GetHeight());
+            const auto ray = UnitRay3f::FromTo(origin, lower_left_corner + u*horizontal + v*vertical);
+            const auto color = GetColor(ray);
             img.SetPixel(x,y, rgbi(color));
         }
     }
