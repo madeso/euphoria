b128cda63f013bf89ec7cda7c9e48661ab415e4d
Gustav <sir.gustav.the.coder@gmail.com>
2020-03-10 07:38:04 +0100

    improved the code

--- core/hashgen_retro.cc
+++ core/hashgen_retro.cc
@@ -28,47 +28,43 @@ namespace
         int half_size = 3
     )
     {
-        auto rand = TGenerator{code};
         const auto size = half_size * 2;
 
         // todo(Gustav): figure out color (randomly?)
-        const auto color = Color::White;
-        const auto bg = Color::Black;
+        const auto foreground_color = Color::White;
+        const auto background_color = Color::Black;
 
         const auto dx = image->GetWidth() / (size - 1);
         const auto dy = image->GetHeight() / size;
 
-        Clear(image, bg);
+        auto draw_rect = [&](const vec2i& top_left)
+        {
+            DrawRect
+            (
+                image,
+                foreground_color,
+                Recti::FromTopLeftWidthHeight
+                (
+                    top_left,
+                    dx+1, dy
+                )
+            );
+        };
+
+        Clear(image, background_color);
+
+        auto generator = TGenerator{code};
 
         for(int y=0; y<size; y+=1)
         for(int x=0; x<half_size; x+=1)
         {
-            const auto r = rand.Next();
-            const auto filled = r < 0.5f;
+            const auto filled = generator.Next() < 0.5f;
             if(filled)
             {
-                DrawRect
-                (
-                    image,
-                    color,
-                    Recti::FromTopLeftWidthHeight
-                    (
-                        vec2i{x*dx, (y+1)*dy},
-                        dx+1, dy
-                    )
-                );
+                draw_rect(vec2i{x*dx, (y+1)*dy});
                 if(x != half_size-1)
                 {
-                    DrawRect
-                    (
-                        image,
-                        color,
-                        Recti::FromTopLeftWidthHeight
-                        (
-                            vec2i{image->GetWidth() - ((x+1)*dx + 2), (y+1)*dy},
-                            dx+1, dy
-                        )
-                    );
+                    draw_rect(vec2i{image->GetWidth() - ((x+1)*dx + 2), (y+1)*dy});
                 }
             }
         }
