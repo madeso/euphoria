3cd9f253bddc1f9bcfb5a75177da41b4b68db910
Gustav <sir.gustav.the.coder@gmail.com>
2020-03-12 16:45:36 +0100

    updated tabletool to new argparse

--- samples/tabletool.cc
+++ samples/tabletool.cc
@@ -23,61 +23,46 @@ StreamToString(std::istream& out)
 int
 main(int argc, char* argv[])
 {
-    std::vector<std::string> files;
-    std::string              format = ",\"";
-    Type                     type   = Type::Simple;
-    CsvTrim                  trim   = CsvTrim::Trim;
+    std::string file;
+    std::string format = ",\"";
+    Type type = Type::Simple;
+    CsvTrim trim = CsvTrim::Trim;
 
     {
         auto parser = argparse::Parser {"csvtool"};
 
-        parser.AddSimple("-format", &format).Help("The CSV format used");
-        parser.AddSimpleFunction("-simple", [&type]() { type = Type::Simple; });
-        parser.AddSimpleFunction(
-                "-notrim", [&trim]() { trim = CsvTrim::DontTrim; });
-        parser.AddSimpleFunction("-grid", [&type]() { type = Type::Grid; });
-        parser.AddVector("files", &files).MetaVar("CSV-file");
-        auto r = parser.Parse(argc, argv);
-        if(r != argparse::ParseResult::Ok)
+        parser.Add("-format", &format).Help("The CSV format used");
+        parser.SetConst("-simple", &type, Type::Simple);
+        parser.SetConst("-grid", &type, Type::Grid);
+        parser.SetConst("-notrim", &trim, CsvTrim::DontTrim);
+        parser.Add("CSV-file", &file);
+        if(const auto r = parser.Parse(argc, argv))
         {
+            return *r;
+        }
+    }
+
+    
+    Table<std::string> table;
+    {
+        std::ifstream stream {file};
+        if(!stream)
+        {
+            std::cerr << "Failed to load " << file << "\n";
             return -1;
         }
+        CsvParserOptions options;
+        options.delim = format[0];
+        options.str = format[1];
+        options.trim = trim;
+        table = TableFromCsv(StreamToString(stream), options);
     }
 
-    int ret = 0;
-
-    bool first = true;
-    for(const auto& f: files)
+    switch(type)
     {
-        if(first)
-        {
-            first = false;
-        }
-        else
-        {
-            std::cout << "\n\n\n";
-        }
-
-        Table<std::string> table;
-        {
-            std::ifstream stream {f};
-            if(!stream)
-            {
-                std::cerr << "Failed to load " << f << "\n";
-                ret = -1;
-            }
-            CsvParserOptions options;
-            options.delim = format[0];
-            options.str   = format[1];
-            options.trim  = trim;
-            table         = TableFromCsv(StreamToString(stream), options);
-        }
-
-        switch(type)
-        {
-        case Type::Simple: PrintTableSimple(std::cout, table); break;
-        case Type::Grid: PrintTableGrid(std::cout, table); break;
-        }
+    case Type::Simple: PrintTableSimple(std::cout, table); break;
+    case Type::Grid: PrintTableGrid(std::cout, table); break;
     }
-    return ret;
+
+    return 0;
 }
