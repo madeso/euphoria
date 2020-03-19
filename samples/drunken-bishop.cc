86ba91b24af26fbe8efa6f8bf0a538d7d2e60d1a
Gustav <sir.gustav.the.coder@gmail.com>
2020-03-19 22:04:32 +0100

    render bishop as image

--- samples/drunken-bishop.cc
+++ samples/drunken-bishop.cc
@@ -4,6 +4,10 @@
 
 #include "core/random.h"
 #include "core/imageops.h"
+#include "core/palette.h"
+#include "core/palette_tableu.h"
+#include "core/image.h"
+#include "core/image_draw.h"
 
 #include "core/io.h"
 
@@ -12,6 +16,65 @@
 #include <iomanip>
 
 using namespace euphoria::core;
+
+
+Table<int>
+GenerateDrunkenBishopTable(Random* random, int width, int height, bool big)
+{
+    auto hash = std::vector<int>{};
+    const int times = big ? 8 : 4;
+    for(int i=0; i<times; i+=1)
+    {
+        const auto codes = ToCodes(ToBytes(random->NextInteger()), true);
+        for(auto c : codes)
+        {
+            hash.emplace_back(c);
+        }
+    }
+    return DrunkenBishop(hash, width, height);
+}
+
+
+Image
+GenerateImage(const Table<int>& table, int scale, const Palette& pal)
+{
+    Image image;
+    image.SetupNoAlphaSupport(scale * table.GetWidth(), scale * table.GetHeight());
+
+    auto draw_rect = [&](const Rgbi& color, const vec2i& top_left)
+    {
+        DrawRect
+        (
+            &image,
+            color,
+            Recti::FromTopLeftWidthHeight
+            (
+                top_left,
+                scale+1, scale
+            )
+        );
+    };
+
+    for(int y=0; y<table.GetHeight(); y+=1)
+    {
+        for(int x=0; x<table.GetWidth(); x+=1)
+        {
+            const auto v =
+            std::max
+            (
+                0,
+                std::min
+                (
+                    table(x,y),
+                    Csizet_to_int(pal.colors.size())
+                )
+            );
+            draw_rect(pal.colors[v], vec2i{x*scale, (y+1)*scale});
+        }
+    }
+
+    return image;
+}
 
 
 int
@@ -29,6 +92,26 @@ main(int argc, char* argv[])
         parser.AddSimple("--height", &height).Help("set the width");
     };
 
+    auto pimg = parser.AddSubParser
+    (
+        "img",
+        "drunken bishop with img style",
+        [&]
+        {
+            auto random = Random{};
+            const auto table = GenerateDrunkenBishopTable(&random, width, height, big);
+            const auto image = GenerateImage
+            (
+                table,
+                10,
+                palette::Tableau_10()
+            );
+            const std::string file_name = "bishop.png";
+            io::ChunkToFile(image.Write(ImageWriteFormat::PNG), file_name);
+        }
+    );
+    add_common(*pimg);
+
     auto precursive = parser.AddSubParser
     (
         "text",
@@ -36,17 +119,7 @@ main(int argc, char* argv[])
         [&]
         {
             auto random = Random{};
-            auto hash = std::vector<int>{};
-            const int times = big ? 8 : 4;
-            for(int i=0; i<times; i+=1)
-            {
-                const auto codes = ToCodes(ToBytes(random.NextInteger()), true);
-                for(auto c : codes)
-                {
-                    hash.emplace_back(c);
-                }
-            }
-            const auto table = DrunkenBishop(hash, width, height);
+            const auto table = GenerateDrunkenBishopTable(&random, width, height, big);
             const auto strs = Collapse(table, GetSshCharacters());
             for(const auto str: strs)
             {
