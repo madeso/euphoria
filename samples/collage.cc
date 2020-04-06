2feab9cc4706246c10b88bce2b6f7ffaed706ded
Gustav <sir.gustav.the.coder@gmail.com>
2020-04-06 20:56:00 +0200

    added todos

--- samples/collage.cc
+++ samples/collage.cc
@@ -17,6 +17,7 @@ using namespace euphoria::core;
 int
 main(int argc, char* argv[])
 {
+    // todo(Gustav): expose as commandline arguments
     std::string output_file = "collage.png";
     auto image_size = Sizei::FromWidthHeight(1014, 1024);
     int padding = 5;
@@ -66,6 +67,8 @@ main(int argc, char* argv[])
         bool all_packed = true; // updated when drawing
 
         // draw new image
+        // todo(Gustav): add option to pack make image smaller than requested
+        // to reduce wasted space and tighly pack the images
         auto composed_image = Image{};
         composed_image.SetupWithAlphaSupport(image_size.width, image_size.height);
         Clear(&composed_image, background_color);
