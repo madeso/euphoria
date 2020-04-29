fd989e95050367c091b430886bf22b1101b117a4
Gustav <sir.gustav.the.coder@gmail.com>
2020-04-29 07:49:53 +0200

    less public/private crap

--- core/bufferbuilder2d.h
+++ core/bufferbuilder2d.h
@@ -8,7 +8,6 @@ namespace euphoria::core
 {
     struct Point
     {
-    public:
         Point(float x, float y, float u, float v);
         Point(const vec2f& apos, const vec2f& avert);
 
@@ -20,22 +19,23 @@ namespace euphoria::core
 
     struct BufferBuilder2d
     {
-    public:
         BufferBuilder2d();
 
         void
         AddVertex(const Point& p);
+
         void
         AddTriangle(unsigned int a, unsigned int b, unsigned int c);
+
         void
         AddQuad(const Point& a, const Point& b, const Point& c, const Point& d);
 
         const std::vector<float>&
         GetVertexData() const;
+
         const std::vector<unsigned int>&
         GetTriangleIndices() const;
 
-    private:
         std::vector<float>        data;
         std::vector<unsigned int> tris;
     };
