3a72bf5c28d3c3582c5721e8c4044a8764704bff
Gustav <sir.gustav.the.coder@gmail.com>
2020-05-02 14:52:11 +0200

    started on 2d sample

---
+++ samples/2d.cc
@@ -0,0 +1,193 @@
+#include "SDL.h"
+
+#include "core/log.h"
+#include "core/str.h"
+#include "core/interpolate.h"
+#include "core/os.h"
+#include "core/ecs-systems.h"
+#include "core/vfs_imagegenerator.h"
+#include "core/vfs_defaultshaders.h"
+#include "core/proto.h"
+#include "core/viewport.h"
+
+#include "render/debuggl.h"
+#include "render/fonts.h"
+#include "render/init.h"
+#include "render/fontcache.h"
+#include "render/scalablesprite.h"
+#include "render/shaderattribute2d.h"
+#include "render/texturecache.h"
+#include "render/viewport.h"
+#include "render/shader.h"
+#include "render/spriterender.h"
+#include "render/viewporthandler.h"
+
+#include "window/key.h"
+#include "window/imguilibrary.h"
+#include "window/imgui_ext.h"
+#include "window/filesystem.h"
+#include "window/sdllibrary.h"
+#include "window/sdlwindow.h"
+#include "window/sdlglcontext.h"
+#include "window/engine.h"
+
+#include "engine/loadworld.h"
+#include "engine/systems.h"
+#include "engine/dukintegration.h"
+#include "engine/dukmathbindings.h"
+#include "engine/dukprint.h"
+#include "engine/input.h"
+#include "engine/objectemplate.h"
+#include "engine/components.h"
+#include "engine/cameradata.h"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_internal.h"
+
+LOG_SPECIFY_DEFAULT_LOGGER("samples-gui")
+
+
+using namespace euphoria::core;
+using namespace euphoria::render;
+using namespace euphoria::window;
+using namespace euphoria::engine;
+
+
+int
+main(int argc, char* argv[])
+{
+    Engine engine;
+    if(const auto ret = engine.Setup(argparse::Arguments::Extract(argc, argv)) != 0; ret != 0)
+    {
+        return ret;
+    }
+
+    engine.file_system->SetWrite
+    (
+        std::make_shared<vfs::FileSystemWriteFolder>(GetCurrentDirectory())
+    );
+
+    TextureCache cache {engine.file_system.get()};
+
+    const auto clear_color = Color::LightGray;
+
+    ViewportHandler viewport_handler;
+
+    int window_width  = 800;
+    int window_height = 600;
+
+    if
+    (
+        engine.CreateWindow
+        (
+            "euphoria 2d demo",
+            window_width,
+            window_height,
+            true
+        ) == false
+    )
+    {
+        return -1;
+    }
+
+    viewport_handler.SetSize(window_width, window_height);
+
+    Shader shader;
+    attributes2d::PrebindShader(&shader);
+    shader.Load(engine.file_system.get(), vfs::FilePath{"~/shaders/sprite"});
+    SpriteRenderer renderer(&shader);
+    FontCache      font_cache {engine.file_system.get(), &cache};
+
+    Use(&shader);
+    shader.SetUniform(shader.GetUniform("image"), 0);
+
+    Uint64 now  = SDL_GetPerformanceCounter();
+    Uint64 last = 0;
+
+    // SDL_StartTextInput();
+
+    bool running = true;
+    constexpr bool show_imgui = true;
+
+    int window_mouse_x = 0;
+    int window_mouse_y = 0;
+    SDL_GetMouseState(&window_mouse_x, &window_mouse_y);
+    bool mouse_lmb_down = false;
+
+    engine.init->Use2d();
+
+    while(running)
+    {
+        last = now;
+        now = SDL_GetPerformanceCounter();
+        const float dt = (now - last) * 1.0f / SDL_GetPerformanceFrequency();
+        SDL_Event e;
+
+        // imgui
+        if(show_imgui)
+        {
+            engine.imgui->StartNewFrame();
+
+            ImGui::Begin("2d");
+            ImGui::End();
+        }
+
+        while(SDL_PollEvent(&e) != 0)
+        {
+            if(e.type == SDL_QUIT)
+            {
+                running = false;
+            }
+
+            if(show_imgui)
+            {
+                engine.imgui->ProcessEvents(&e);
+            }
+
+            if(engine.HandleResize(e, &window_width, &window_height))
+            {
+                viewport_handler.SetSize(window_width, window_height);
+            }
+
+            if(e.type == SDL_MOUSEMOTION)
+            {
+                window_mouse_x = e.motion.x;
+                window_mouse_y = e.motion.y;
+            }
+            else if(e.type == SDL_KEYUP || e.type == SDL_KEYDOWN)
+            {
+                const bool down = e.type == SDL_KEYDOWN;
+                const auto key = ToKey(e.key.keysym);
+                if(down && key == Key::ESCAPE)
+                {
+                    running = false;
+                }
+            }
+            else if(e.type == SDL_MOUSEBUTTONDOWN
+                    || e.type == SDL_MOUSEBUTTONUP)
+            {
+                const bool down = e.type == SDL_MOUSEBUTTONDOWN;
+                window_mouse_x = e.button.x;
+                window_mouse_y = e.button.y;
+                if(e.button.button == SDL_BUTTON_LEFT)
+                {
+                    mouse_lmb_down = down;
+                }
+            }
+            else if(e.type == SDL_TEXTINPUT)
+            {
+                // const std::string& input = e.text.text;
+            }
+        }
+        
+        engine.init->ClearScreen(clear_color);
+        if(show_imgui)
+        {
+            engine.imgui->Render();
+        }
+        SDL_GL_SwapWindow(engine.window->window);
+    }
+
+    return 0;
+}
+
