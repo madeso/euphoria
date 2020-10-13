ce633a2448fbb4b1c3f5e3fae47dc67d76949eb8
Gustav <sir.gustav.the.coder@gmail.com>
2020-10-13 18:53:28 +0200

    added (untested) util function found on the internets

--- src/core/stdutils.h
+++ src/core/stdutils.h
@@ -86,6 +86,15 @@ namespace euphoria::core
         return v.end();
     }
 
+
+    // convert a enum class to it's underlying (int) type
+    // src: https://twitter.com/idoccor/status/1314664849276899328
+    template<typename E>
+    constexpr typename std::underlying_type<E>::type ToBase(E e) noexcept
+    {
+        return static_cast<typename std::underlying_type<E>::type>(e);
+    }
+
 }  // namespace euphoria::core
 
 #endif  // EUPHORIA_STDUTILS_H
