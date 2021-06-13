e829022cf72b7a5e37cefd28cba0d4068c670aac
Gustav <sir.gustav.the.coder@gmail.com>
2021-06-13 10:01:34 +0200

    merged list-includes to code.py

--- tools/compile_commands.py
+++ tools/compile_commands.py
@@ -17,6 +17,9 @@ import statistics
 import itertools
 
 
+COMPILE_COMMANDS_FILE_NAME = 'compile_commands.json'
+
+
 class CompileCommand:
     def __init__(self, directory: str, command: str):
         self.directory = directory
@@ -47,8 +50,22 @@ def find_build_root(root):
     """
     for relative_build in ['build', 'build/debug-clang']:
         build = os.path.join(root, relative_build)
-        compile_commands_json = os.path.join(build, 'compile_commands.json')
+        compile_commands_json = os.path.join(build, COMPILE_COMMANDS_FILE_NAME)
         if os.path.isfile(compile_commands_json):
             return build
 
     return None
+
+
+def add_argument(sub):
+    sub.add_argument('--compile-commands', dest='cc', default=None, help="the path to compile commands")
+
+
+def get_argument_or_none(args) -> typing.Optional[str]:
+    if args.cc is None:
+        build = find_build_root(os.getcwd())
+        if build is None:
+            return None
+        else:
+            return os.path.join(build, COMPILE_COMMANDS_FILE_NAME)
+    return args.cc
