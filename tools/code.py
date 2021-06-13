7e11915220d0e628e451d55b0712c6f7312670ae
Gustav <sir.gustav.the.coder@gmail.com>
2021-06-13 10:06:24 +0200

    list indents merged with code.py

--- tools/code.py
+++ tools/code.py
@@ -189,8 +189,48 @@ def get_line_count(path: str, discard_empty: bool) -> int:
     return -1
 
 
+def get_max_indent(path: str, discard_empty: bool) -> int:
+    with open(path, 'r') as handle:
+        count = 0
+        for line in handle:
+            if discard_empty and len(line.strip()) == 0:
+                pass
+            else:
+                count = max(count, len(line) - len(line.lstrip()))
+        return count
+
+    return -1
+
+
 ###################################################################################################
 ## handlers
+
+def handle_list_indents(args):
+    stats = {}
+    files = 0
+    each = args.each
+
+    for patt in args.files:
+        for file in list_files_in_folder(patt, ['.h', '.cc']):
+            files += 1
+
+            count = get_max_indent(file, args.discard_empty)
+
+            index = count if each <= 1 else count - (count % each)
+            if index in stats:
+                stats[index].append(file)
+            else:
+                stats[index] = [file]
+
+    print(f'Found {files} files.')
+    for count,files in sorted(stats.items(), key=lambda item: item[0]):
+        c = len(files)
+        count_str = f'{count}' if each <= 1 else f'{count}-{count+each-1}'
+        if args.show and c < 3:
+            print(f'{count_str}: {files}')
+        else:
+            print(f'{count_str}: {c}')
+
 
 def handle_list(args):
     compile_commands_arg = cc.get_argument_or_none(args)
@@ -314,6 +354,13 @@ def main():
     sub.add_argument('--group', action='store_true', help="group output")
     sub.add_argument('--cluster', action='store_true', help="group output into clusters")
     sub.set_defaults(func=handle_gv)
+
+    sub = sub_parsers.add_parser('list-indents', help='list the files with the maximum indents')
+    sub.add_argument('files', nargs='+')
+    sub.add_argument('--each', type=int, default=1)
+    sub.add_argument('--show', action='store_true')
+    sub.add_argument('--include-empty', dest='discard_empty', action='store_false')
+    sub.set_defaults(func=handle_list_indents)
 
     args = parser.parse_args()
     if args.command_name is not None:
