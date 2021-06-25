285b1b53a6879ca8b397dd406f1f0d0dad5be171
Gustav <sir.gustav.the.coder@gmail.com>
2021-06-25 08:47:52 +0200

    clang-tidy: google-readability-casting

--- libs/window/src/window/imgui_extra.cc
+++ libs/window/src/window/imgui_extra.cc
@@ -310,7 +310,7 @@ namespace euphoria::window::imgui
         if((col & IM_COL32_A_MASK) == 0)
         { return; }
 
-        const float a_max = IM_PI * 2.0f * ((float)num_segments - 1.0f) / (float)num_segments;
+        const float a_max = IM_PI * 2.0f * (core::c_int_to_float(num_segments) - 1.0f) / core::c_int_to_float(num_segments);
         draw_list->PathArcTo
         (
             centre,
