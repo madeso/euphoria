edf4b2c786a876355f2e2793431c7dbfe144704e
Gustav <sir.gustav.the.coder@gmail.com>
2021-12-09 23:01:34 +0100

    build: updated buildtools to latest and made sure it compiles with vs20222

--- tools/windows.py
+++ tools/windows.py
@@ -1,104 +1,10 @@
 #!/usr/bin/env python3
 """build script for windows for euphoria"""
 
-import os
-import subprocess
-import argparse
-import buildtools.deps as btdeps
-import buildtools.cmake as btcmake
-import buildtools.args as btargs
-import buildtools.visualstudio as btstudio
 
+import buildtools.windows as build
 
-###############################################################################
-
-
-ROOT_FOLDER = os.getcwd()
-BUILD_FOLDER = os.path.join(ROOT_FOLDER, 'build', 'euph')
-DEPENDENCY_FOLDER = os.path.join(ROOT_FOLDER, 'build', 'deps')
-SDL2_FOLDER = os.path.join(DEPENDENCY_FOLDER, 'sdl2')
-SDL2_BUILD_FOLDER = os.path.join(SDL2_FOLDER, 'cmake-build')
-ASSIMP_FOLDER = os.path.join(DEPENDENCY_FOLDER, 'assimp')
-ASSIMP_INSTALL_FOLDER = os.path.join(ASSIMP_FOLDER, 'cmake-install')
-
-
-###############################################################################
-
-
-def generate_cmake_project(generator):
-    """generate the euphoira project"""
-    project = btcmake.CMake(BUILD_FOLDER, ROOT_FOLDER, generator)
-    project.add_argument('SDL2_HINT_ROOT', SDL2_FOLDER)
-    project.add_argument('SDL2_HINT_BUILD', SDL2_BUILD_FOLDER)
-    project.add_argument('ASSIMP_ROOT_DIR', ASSIMP_INSTALL_FOLDER)
-    if 'PYTHON' in os.environ:
-        python_exe = os.environ['PYTHON']+'\\python.exe'
-        project.add_argument('PYTHON_EXECUTABLE:FILEPATH', python_exe)
-    return project
-
-
-def run_install(compiler, platform, generator):
-    """install euphoria dependencies"""
-    btdeps.install_dependency_assimp(DEPENDENCY_FOLDER, ASSIMP_FOLDER,
-                                     ASSIMP_INSTALL_FOLDER, generator)
-    btdeps.install_dependency_sdl2(DEPENDENCY_FOLDER, SDL2_FOLDER,
-                                   SDL2_BUILD_FOLDER, generator)
-
-
-def run_cmake(platform, generator, only_print):
-    """configure the euphoria cmake project"""
-    generate_cmake_project(generator).config(only_print)
-
-
-def run(args) -> str:
-    """run a terminal and return the output or error"""
-    try:
-        return subprocess.check_output(args, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as error:
-        print('Failed to run {} {}'.format(error.cmd, error.returncode))
-        return error.stdout
-
-
-###############################################################################
-
-
-def on_cmd_install(arg):
-    """callback for install command"""
-    compiler = btargs.get_compiler(arg)
-    platform = btargs.get_platform(arg)
-    generator = btstudio.visual_studio_generator(compiler, platform)
-
-    run_install(compiler, platform, generator)
-
-
-def on_cmd_cmake(arg):
-    """callback for cmake command"""
-    compiler = btargs.get_compiler(arg)
-    platform = btargs.get_platform(arg)
-    generator = btstudio.visual_studio_generator(compiler, platform)
-
-    run_cmake(platform, generator, arg.print)
-
-
-def on_cmd_dev(arg):
-    """callback for dev command"""
-    compiler = btargs.get_compiler(arg)
-    platform = btargs.get_platform(arg)
-    generator = btstudio.visual_studio_generator(compiler, platform)
-
-    run_install(compiler, platform, generator)
-    run_cmake(platform, generator)
-
-
-def on_cmd_build(arg):
-    """callback for build cmd"""
-    compiler = btargs.get_compiler(arg)
-    platform = btargs.get_platform(arg)
-    generator = btstudio.visual_studio_generator(compiler, platform)
-
-    generate_cmake_project(generator).build()
-
-
+# todo(Gustav): add test command to windows build
 def on_cmd_test(_):
     """callback for test cmd"""
     # todo(Gustav): figure out how to run all tests
@@ -115,50 +21,18 @@ def on_cmd_test(_):
     print('junit file written!')
 
 
-###############################################################################
-
-
-def add_options(parser):
-    """add compiler and platform options"""
-    btargs.add_compiler(parser)
-    btargs.add_platform(parser)
-
-
-###############################################################################
-
-
-def main():
-    """entry point for script"""
-    parser = argparse.ArgumentParser(description='Does the windows build')
-    parser.set_defaults(func=None)
-    subparsers = parser.add_subparsers()
-
-    install_parser = subparsers.add_parser('install')
-    install_parser.set_defaults(func=on_cmd_install)
-    add_options(install_parser)
-
-    cmmake_parser = subparsers.add_parser('cmake')
-    cmmake_parser.add_argument('--print', action='store_true')
-    cmmake_parser.set_defaults(func=on_cmd_cmake)
-    add_options(cmmake_parser)
-
-    # dev is install + cmake
-    dev_parser = subparsers.add_parser('dev')
-    dev_parser.set_defaults(func=on_cmd_dev)
-    add_options(dev_parser)
-
-    build_parser = subparsers.add_parser('build')
-    build_parser.set_defaults(func=on_cmd_build)
-    add_options(build_parser)
-
+def add_tests(subparsers):
     test_parser = subparsers.add_parser('test')
     test_parser.set_defaults(func=on_cmd_test)
 
-    arg = parser.parse_args()
-    if arg.func is None:
-        parser.print_help()
-    else:
-        arg.func(arg)
+
+def main():
+    """main function"""
+    data = build.default_data('euphoria')
+    build.add_dependency_sdl2(data)
+    build.add_dependency_python(data)
+    build.add_dependency_assimp(data)
+    build.main(data)
 
 
 if __name__ == "__main__":
