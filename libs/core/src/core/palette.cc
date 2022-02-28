f687dce7e65d22b66b98accc11d91528c15557a7
Gustav <sir.gustav.the.coder@gmail.com>
2022-02-28 23:38:51 +0100

    fix: prefer make_static otherwise we might use already deleted values

--- libs/core/src/core/palette.cc
+++ libs/core/src/core/palette.cc
@@ -159,73 +159,67 @@ namespace euphoria::core
         const Palette&
         dawnbringer()
         {
-            static const auto p = Palette
-            {
-                "dawnbringer",
-                std::array
-                {
-                        hex(0x140C1C),
-                        hex(0x442434),
-                        hex(0x30346D),
-                        hex(0x4E4A4E),
-                        hex(0x854C30),
-                        hex(0x346524),
-                        hex(0xD04648),
-                        hex(0x757161),
-                        hex(0x597DCE),
-                        hex(0xD27D2C),
-                        hex(0x8595A1),
-                        hex(0x6DAA2C),
-                        hex(0xD2AA99),
-                        hex(0x6DC2CA),
-                        hex(0xDAD45E),
-                        hex(0xDEEED6),
-                }
-            };
-            return p;
+            static const auto p = make_static_palette
+            (
+                "Dawnbringer",
+                hex(0x140C1C),
+                hex(0x442434),
+                hex(0x30346D),
+                hex(0x4E4A4E),
+                hex(0x854C30),
+                hex(0x346524),
+                hex(0xD04648),
+                hex(0x757161),
+                hex(0x597DCE),
+                hex(0xD27D2C),
+                hex(0x8595A1),
+                hex(0x6DAA2C),
+                hex(0xD2AA99),
+                hex(0x6DC2CA),
+                hex(0xDAD45E),
+                hex(0xDEEED6)
+            );
+            return *p;
         }
 
         const Palette&
         named_colors()
         {
-            static const auto p = Palette
-            {
-                "named",
-                std::array
-                {
-                    Rgbi{NamedColor::white},
-                    Rgbi{NamedColor::light_gray},
-                    Rgbi{NamedColor::gray},
-                    Rgbi{NamedColor::dark_gray},
-                    Rgbi{NamedColor::black},
-                    Rgbi{NamedColor::red},
-                    Rgbi{NamedColor::pure_red},
-                    Rgbi{NamedColor::blue},
-                    Rgbi{NamedColor::pure_blue},
-                    Rgbi{NamedColor::light_blue},
-                    Rgbi{NamedColor::normal_blue},
-                    Rgbi{NamedColor::cornflower_blue},
-                    Rgbi{NamedColor::green},
-                    Rgbi{NamedColor::pure_green},
-                    Rgbi{NamedColor::light_green},
-                    Rgbi{NamedColor::yellow},
-                    Rgbi{NamedColor::pure_yellow},
-                    Rgbi{NamedColor::orange},
-                    Rgbi{NamedColor::pure_orange},
-                    Rgbi{NamedColor::brown},
-                    Rgbi{NamedColor::pure_brown},
-                    Rgbi{NamedColor::purple},
-                    Rgbi{NamedColor::pure_purple},
-                    Rgbi{NamedColor::pink},
-                    Rgbi{NamedColor::pure_pink},
-                    Rgbi{NamedColor::pure_beige},
-                    Rgbi{NamedColor::tan},
-                    Rgbi{NamedColor::pure_tan},
-                    Rgbi{NamedColor::cyan},
-                    Rgbi{NamedColor::pure_cyan}
-                }
-            };
-            return p;
+            static const auto p = make_static_palette
+            (
+                "Named colors",
+                Rgbi{NamedColor::white},
+                Rgbi{NamedColor::light_gray},
+                Rgbi{NamedColor::gray},
+                Rgbi{NamedColor::dark_gray},
+                Rgbi{NamedColor::black},
+                Rgbi{NamedColor::red},
+                Rgbi{NamedColor::pure_red},
+                Rgbi{NamedColor::blue},
+                Rgbi{NamedColor::pure_blue},
+                Rgbi{NamedColor::light_blue},
+                Rgbi{NamedColor::normal_blue},
+                Rgbi{NamedColor::cornflower_blue},
+                Rgbi{NamedColor::green},
+                Rgbi{NamedColor::pure_green},
+                Rgbi{NamedColor::light_green},
+                Rgbi{NamedColor::yellow},
+                Rgbi{NamedColor::pure_yellow},
+                Rgbi{NamedColor::orange},
+                Rgbi{NamedColor::pure_orange},
+                Rgbi{NamedColor::brown},
+                Rgbi{NamedColor::pure_brown},
+                Rgbi{NamedColor::purple},
+                Rgbi{NamedColor::pure_purple},
+                Rgbi{NamedColor::pink},
+                Rgbi{NamedColor::pure_pink},
+                Rgbi{NamedColor::pure_beige},
+                Rgbi{NamedColor::tan},
+                Rgbi{NamedColor::pure_tan},
+                Rgbi{NamedColor::cyan},
+                Rgbi{NamedColor::pure_cyan}
+            );
+            return *p;
         }
     }
 }
