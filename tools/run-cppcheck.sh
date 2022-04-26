8443875d2e2e432c8bd3b79b20e1adbb6bf8533f
Gustav <sir.gustav.the.coder@gmail.com>
2022-04-26 11:31:56 +0200

    build: run cppcheck locally via tools

---
+++ tools/run-cppcheck.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cppcheck --enable=all --inconclusive --error-exitcode=0 libs/ apps/ 2> cppcheck-result.txt
+
+# remove colors: https://stackoverflow.com/questions/17998978/removing-colors-from-output
+cat cppcheck-result.txt | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g'  > cppcheck-result-fix.txt
+rm cppcheck-result.txt
+mv cppcheck-result-fix.txt cppcheck-result.txt
