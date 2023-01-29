67e6494647f7eae487ebf549d187e5f5c9727acb
Gustav <sir.gustav.the.coder@gmail.com>
2023-01-29 18:43:54 +0100

    Formated mat4 test

--- libs/core/src/core/mat4.test.cc
+++ libs/core/src/core/mat4.test.cc
@@ -10,8 +10,13 @@ using namespace euphoria::tests;
 
 TEST_CASE("mat4-colmajor", "[mat]")
 {
-    const auto m = euco::mat4i::from_col_major(
-            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+    const auto m = euco::mat4i::from_col_major
+    (
+        0, 1, 2, 3,
+        4, 5, 6, 7,
+        8, 9, 10, 11,
+        12, 13, 14, 15
+    );
     REQUIRE(euco::vec4i(0, 1, 2, 3) == m.get_column(0));
     REQUIRE(euco::vec4i(4, 5, 6, 7) == m.get_column(1));
     REQUIRE(euco::vec4i(8, 9, 10, 11) == m.get_column(2));
@@ -20,8 +25,13 @@ TEST_CASE("mat4-colmajor", "[mat]")
 
 TEST_CASE("mat4-rowmajor", "[mat]")
 {
-    const auto m = euco::mat4i::from_row_major(
-            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+    const auto m = euco::mat4i::from_row_major
+    (
+        0, 1, 2, 3,
+        4, 5, 6, 7,
+        8, 9, 10, 11,
+        12, 13, 14, 15
+    );
     REQUIRE(euco::vec4i(0, 4, 8, 12) == m.get_column(0));
     REQUIRE(euco::vec4i(1, 5, 9, 13) == m.get_column(1));
     REQUIRE(euco::vec4i(2, 6, 10, 14) == m.get_column(2));
@@ -30,8 +40,13 @@ TEST_CASE("mat4-rowmajor", "[mat]")
 
 TEST_CASE("mat4-rowmajor_row", "[mat]")
 {
-    const auto m = euco::mat4i::from_row_major(
-            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+    const auto m = euco::mat4i::from_row_major
+    (
+        0, 1, 2, 3,
+        4, 5, 6, 7,
+        8, 9, 10, 11,
+        12, 13, 14, 15
+    );
     REQUIRE(euco::vec4i(0, 1, 2, 3) == m.get_row(0));
     REQUIRE(euco::vec4i(4, 5, 6, 7) == m.get_row(1));
     REQUIRE(euco::vec4i(8, 9, 10, 11) == m.get_row(2));
@@ -40,8 +55,13 @@ TEST_CASE("mat4-rowmajor_row", "[mat]")
 
 TEST_CASE("mat4-identity", "[mat]")
 {
-    const auto m = euco::mat4i::from_row_major(
-            1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+    const auto m = euco::mat4i::from_row_major
+    (
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+    );
     const auto i = euco::mat4i::identity();
     REQUIRE(i.get_column(0) == m.get_column(0));
     REQUIRE(i.get_column(1) == m.get_column(1));
@@ -51,29 +71,49 @@ TEST_CASE("mat4-identity", "[mat]")
 
 TEST_CASE("mat4-index", "[mat]")
 {
-    const auto m = euco::mat4i::from_row_major(
-            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+    const auto m = euco::mat4i::from_row_major
+    (
+        0, 1, 2, 3,
+        4, 5, 6, 7,
+        8, 9, 10, 11,
+        12, 13, 14, 15
+    );
     REQUIRE(3 == m(0, 3));
     REQUIRE(15 == m(3, 3));
 }
 
 TEST_CASE("mat4-major", "[mat]")
 {
-    const auto m = euco::mat4i::from_row_major(
-                           0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
-                           .get_major();
+    const auto m = euco::mat4i::from_row_major
+        (
+            0, 1, 2, 3,
+            4, 5, 6, 7,
+            8, 9, 10, 11,
+            12, 13, 14, 15
+        )
+        .get_major();
     REQUIRE(euco::vec4i(0, 5, 10, 15) == m);
 }
 
 
 TEST_CASE("mat4-transposed", "[mat]")
 {
-    const auto m = euco::mat4i::from_row_major(
-                           0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
-                           .get_transposed();
-    REQUIRE(m
-            == euco::mat4i::from_col_major(
-                    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15));
+    const auto m = euco::mat4i::from_row_major
+        (
+            0, 1, 2, 3,
+            4, 5, 6, 7,
+            8, 9, 10, 11,
+            12, 13, 14, 15
+        )
+        .get_transposed();
+    REQUIRE(m == euco::mat4i::from_col_major
+        (
+            0, 1, 2, 3,
+            4, 5, 6, 7,
+            8, 9, 10, 11,
+            12, 13, 14, 15
+        )
+    );
 }
 
 TEST_CASE("mat4-axis", "[mat]")
@@ -84,131 +124,70 @@ TEST_CASE("mat4-axis", "[mat]")
     REQUIRE(euco::vec3i(0, 0, 1) == m.get_z_axis());
 }
 
-TEST_CASE("mat4-multiply", "[mat]")
+TEST_CASE("mat4-math", "[mat]")
 {
-    const auto m = euco::mat4i::from_row_major(
-                           0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
-                   * euco::mat4i::from_row_major(
-                           16,
-                           17,
-                           18,
-                           19,
-                           20,
-                           21,
-                           22,
-                           23,
-                           24,
-                           25,
-                           26,
-                           27,
-                           28,
-                           29,
-                           30,
-                           31);
-    // thanks wolfram alpha for the result
-    // http://www.wolframalpha.com/widgets/view.jsp?id=cc71c2e95a80c217564d530fd8297b0e
-    // simplify({{0,1,2,3},{4,5,6,7},{8,9,10,11},{12,13,14,15}}.{{16,17,18,19},{20,21,22,23},{24,25,26,27},{28,29,30,31}})
-    REQUIRE(m
-            == euco::mat4i::from_row_major(
-                    152,
-                    158,
-                    164,
-                    170,
-                    504,
-                    526,
-                    548,
-                    570,
-                    856,
-                    894,
-                    932,
-                    970,
-                    1208,
-                    1262,
-                    1316,
-                    1370));
-}
+    const auto lhs = euco::mat4i::from_row_major
+    (
+        0, 1, 2, 3,
+        4, 5, 6, 7,
+        8, 9, 10, 11,
+        12, 13, 14, 15
+    );
+    const auto rhs = euco::mat4i::from_row_major
+    (
+        16, 17, 18, 19,
+        20, 21, 22, 23,
+        24, 25, 26, 27,
+        28, 29, 30, 31
+    );
 
-TEST_CASE("mat4-add", "[mat]")
-{
-    const auto m = euco::mat4i::from_row_major(
-                           0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
-                   + euco::mat4i::from_row_major(
-                           16,
-                           17,
-                           18,
-                           19,
-                           20,
-                           21,
-                           22,
-                           23,
-                           24,
-                           25,
-                           26,
-                           27,
-                           28,
-                           29,
-                           30,
-                           31);
-    REQUIRE(m
-            == euco::mat4i::from_row_major(
-                    16,
-                    18,
-                    20,
-                    22,
-                    24,
-                    26,
-                    28,
-                    30,
-                    32,
-                    34,
-                    36,
-                    38,
-                    40,
-                    42,
-                    44,
-                    46));
-}
+    SECTION("multiply")
+    {
+        const auto m = lhs * rhs;
+        // thanks wolfram alpha for the result
+        // http://www.wolframalpha.com/widgets/view.jsp?id=cc71c2e95a80c217564d530fd8297b0e
+        // simplify({{0,1,2,3},{4,5,6,7},{8,9,10,11},{12,13,14,15}}.{{16,17,18,19},{20,21,22,23},{24,25,26,27},{28,29,30,31}})
+        REQUIRE
+        (
+            m == euco::mat4i::from_row_major
+            (
+                152, 158, 164, 170,
+                504, 526, 548, 570,
+                856, 894, 932, 970,
+                1208, 1262, 1316, 1370
+            )
+        );
+    }
 
-TEST_CASE("mat4-sub", "[mat]")
-{
-    const auto m
-            = euco::mat4i::from_row_major(
-                      16,
-                      17,
-                      18,
-                      19,
-                      20,
-                      21,
-                      22,
-                      23,
-                      24,
-                      25,
-                      26,
-                      27,
-                      28,
-                      29,
-                      30,
-                      31)
-              - euco::mat4i::from_row_major(
-                      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
-    REQUIRE(m
-            == euco::mat4i::from_row_major(
-                    16,
-                    16,
-                    16,
-                    16,
-                    16,
-                    16,
-                    16,
-                    16,
-                    16,
-                    16,
-                    16,
-                    16,
-                    16,
-                    16,
-                    16,
-                    16));
+    SECTION("add")
+    {
+        const auto m = lhs + rhs;
+        REQUIRE
+        (
+            m == euco::mat4i::from_row_major
+            (
+                16, 18, 20, 22,
+                24, 26, 28, 30,
+                32, 34, 36, 38,
+                40, 42, 44, 46
+            )
+        );
+    }
+
+    SECTION("sub")
+    {
+        const auto m = rhs - lhs;
+        REQUIRE
+        (
+            m == euco::mat4i::from_row_major
+            (
+                16, 16, 16, 16,
+                16, 16, 16, 16,
+                16, 16, 16, 16,
+                16, 16, 16, 16
+            )
+        );
+    }
 }
 
 
@@ -245,9 +224,8 @@ TEST_CASE("mat4-inverse", "[mat]")
 
 TEST_CASE("mat4-TestTransformation", "[mat]")
 {
-    REQUIRE(euco::vec3i(1, 2, 3)
-            == euco::mat4i::from_translation(euco::vec3i(1, 2, 3))
-                       .get_translation());
+    REQUIRE(euco::vec3i(1, 2, 3) ==
+        euco::mat4i::from_translation(euco::vec3i(1, 2, 3)).get_translation());
 }
 
 
@@ -271,8 +249,11 @@ TEST_CASE("mat4-TestIn", "[mat]")
 TEST_CASE("mat4-test", "[mat]")
 {
     const euco::mat4f start = euco::mat4f::identity();
-    euco::AxisAngle aa = euco::AxisAngle::right_hand_around(
-            euco::unit3f::up(), euco::angle::from_degrees(-90));
+    const euco::AxisAngle aa = euco::AxisAngle::right_hand_around
+    (
+        euco::unit3f::up(),
+        euco::angle::from_degrees(-90)
+    );
     const euco::vec3f to_transform(0, 0, -5);
     const euco::vec3f result(5, 0, 0);
 
@@ -287,63 +268,88 @@ TEST_CASE("mat4-test", "[mat]")
 TEST_CASE("mat4-TestCombined_RT", "[mat]")
 {
     const auto r = euco::mat4f::identity()
-                           .rotate(euco::AxisAngle::right_hand_around(
-                                   euco::unit3f::up(),
-                                   euco::angle::from_degrees(-90)))
-                           .translate(euco::vec3f(0, 0, -5))
-                           .get_transform_point(euco::vec3f(0, 0, 0));
+        .rotate
+        (
+            euco::AxisAngle::right_hand_around
+            (
+                euco::unit3f::up(),
+                euco::angle::from_degrees(-90)
+            )
+        )
+        .translate(euco::vec3f(0, 0, -5))
+        .get_transform_point(euco::vec3f(0, 0, 0))
+        ;
     REQUIRE(r == approx(euco::vec3f(5, 0, 0)));
 }
 
 TEST_CASE("mat4-TestCombined2_RT", "[mat]")
 {
-    const auto r
-            = euco::mat4f::identity()
-                      .rotate(euco::AxisAngle::right_hand_around(
-                              euco::unit3f::up(), euco::angle::from_degrees(90)))
-                      .translate(euco::vec3f(0, 0, -5))
-                      .get_transform_point(euco::vec3f(0, 0, 0));
+    const auto r =
+        euco::mat4f::identity()
+        .rotate
+        (
+            euco::AxisAngle::right_hand_around
+            (
+                euco::unit3f::up(), euco::angle::from_degrees(90)
+            )
+        )
+        .translate(euco::vec3f(0, 0, -5))
+        .get_transform_point(euco::vec3f(0, 0, 0))
+        ;
     REQUIRE(r == approx(euco::vec3f(-5, 0, 0)));
 }
 
 TEST_CASE("mat4-TestCombined_TR", "[mat]")
 {
     const auto r = euco::mat4f::identity()
-                           .translate(euco::vec3f(0, 0, 5))
-                           .rotate(euco::AxisAngle::right_hand_around(
-                                   euco::unit3f::up(),
-                                   euco::angle::from_degrees(-90)))
-                           .get_transform_point(euco::vec3f(0, 0, 0));
+        .translate(euco::vec3f(0, 0, 5))
+        .rotate
+        (
+            euco::AxisAngle::right_hand_around
+            (
+                euco::unit3f::up(),
+                euco::angle::from_degrees(-90)
+            )
+        )
+        .get_transform_point(euco::vec3f(0, 0, 0))
+        ;
     REQUIRE(r == approx(euco::vec3f(0, 0, 5)));
 }
 
 TEST_CASE("mat4-TestTranslation", "[mat]")
 {
     const auto r = euco::mat4i::identity()
-                           .translate(euco::vec3i(1, 2, 3))
-                           .get_transform_point(euco::vec3i(7, 8, 9));
+        .translate(euco::vec3i(1, 2, 3))
+        .get_transform_point(euco::vec3i(7, 8, 9))
+        ;
     REQUIRE(r == euco::vec3i(8, 10, 12));
 }
 
 
 TEST_CASE("mat4-TestIentityTransform", "[mat]")
 {
-    const auto r
-            = euco::mat4i::identity().get_transform_point(euco::vec3i(1, 2, 3));
+    const auto r = euco::mat4i::identity()
+        .get_transform_point(euco::vec3i(1, 2, 3))
+        ;
     REQUIRE(r == euco::vec3i(1, 2, 3));
 }
 
 TEST_CASE("mat4-TestIentityMultiply", "[mat]")
 {
     REQUIRE(euco::mat4i::identity() * euco::mat4i::identity()
-            == euco::mat4i::identity());
+        == euco::mat4i::identity());
 }
 
 TEST_CASE("mat4-TestVec4Multiply", "[mat]")
 {
-    const auto m = euco::mat4i::from_row_major(
-                           0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
-                   * euco::vec4i(16, 17, 18, 19);
+    const auto lhs = euco::mat4i::from_row_major
+    (
+        0, 1, 2, 3,
+        4, 5, 6, 7,
+        8, 9, 10, 11,
+        12, 13, 14, 15
+    );
+    const auto m = lhs * euco::vec4i(16, 17, 18, 19);
     // simplify({{0,1,2,3},{4,5,6,7},{8,9,10,11},{12,13,14,15}}.{16,17,18,19})
     REQUIRE(m == euco::vec4i(110, 390, 670, 950));
 }
