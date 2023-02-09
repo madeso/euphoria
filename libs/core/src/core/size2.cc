4e8732191ec0801798e62ff5b9141e32798755be
Gustav <sir.gustav.the.coder@gmail.com>
2023-02-09 23:20:28 +0100

    Remove templates from size2

--- libs/core/src/core/size2.cc
+++ libs/core/src/core/size2.cc
@@ -1,2 +1,359 @@
 #include "core/size2.h"
 
+
+namespace euphoria::core
+{
+
+    size2f::size2f()
+        : width(-1)
+        , height(-1)
+    {
+    }
+
+    [[nodiscard]] size2f
+    size2f::create_from_width_height(float w, float h)
+    {
+        return size2f(w, h);
+    }
+
+    [[nodiscard]] size2f
+    size2f::create_from_height_width(float h, float w)
+    {
+        return size2f(w, h);
+    }
+
+    [[nodiscard]] size2f
+    size2f::create_from_square(float s)
+    {
+        return size2f(s, s);
+    }
+
+    void
+    size2f::operator+=(const size2f& rhs)
+    {
+        width += rhs.width;
+        height += rhs.height;
+    }
+
+    void
+    size2f::operator-=(const size2f& rhs)
+    {
+        width -= rhs.width;
+        height -= rhs.height;
+    }
+
+    void
+    size2f::operator*=(float rhs)
+    {
+        width *= rhs;
+        height *= rhs;
+    }
+
+    void
+    size2f::operator/=(float rhs)
+    {
+        width /= rhs;
+        height /= rhs;
+    }
+
+    vec2<float>
+    size2f::calculate_center_offset_for(const size2f& o)
+    {
+        const float x = (width - o.width) / 2;
+        const float y = (height - o.height) / 2;
+        return vec2<float>(x, y);
+    }
+
+    size2f::size2f(float w, float h)
+        : width(w)
+        , height(h)
+    {
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////
+
+
+    std::string
+    CustomArgparser<size2f>::to_string(const size2f& s)
+    {
+        return "{}x{}"_format(s.width, s.height);
+    }
+
+    Result<size2f>
+    CustomArgparser<size2f>::parse(const std::string& value)
+    {
+        using R = Result<size2f>;
+        const auto values = split(value, 'x');
+        const auto xes = values.size();
+        if(xes != 2)
+        {
+            if(xes < 2)
+            {
+                return R::create_error
+                (
+                    "{} contains less than one x"_format(value)
+                );
+            }
+            else
+            {
+                return R::create_error
+                (
+                    "{} contains more than one x: {}"_format(value, xes-1)
+                );
+            }
+        }
+
+        auto parse_hs = [&](int index)
+        {
+            return argparse::default_parse_function<float>
+            (
+                trim(values[index])
+            );
+        };
+        const auto lhs = parse_hs(0);
+        const auto rhs = parse_hs(1);
+
+        if(!lhs) { return R::create_error(lhs.get_error()); }
+        if(!rhs) { return R::create_error(rhs.get_error()); }
+
+        return R::create_value(size2f::create_from_width_height(*lhs, *rhs));
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    size2f
+    min(const size2f lhs, const size2f rhs)
+    {
+        return size2f::create_from_width_height
+        (
+            min(lhs.width, rhs.width), min(lhs.height, rhs.height)
+        );
+    }
+
+    size2f
+    max(const size2f lhs, const size2f rhs)
+    {
+        return size2f::create_from_width_height
+        (
+            max(lhs.width, rhs.width), max(lhs.height, rhs.height)
+        );
+    }
+
+    size2f
+    operator+(const size2f& lhs, const size2f& rhs)
+    {
+        size2f t = lhs;
+        t += rhs;
+        return t;
+    }
+
+    size2f
+    operator-(const size2f& lhs, const size2f& rhs)
+    {
+        size2f t = lhs;
+        t -= rhs;
+        return t;
+    }
+
+    size2f operator*(const size2f& lhs, float rhs)
+    {
+        size2f t = lhs;
+        t *= rhs;
+        return t;
+    }
+
+    size2f operator*(float lhs, const size2f& rhs)
+    {
+        size2f t = rhs;
+        t *= lhs;
+        return t;
+    }
+
+    size2f
+    operator/(const size2f& lhs, float rhs)
+    {
+        size2f t = lhs;
+        t /= rhs;
+        return t;
+    }
+
+    std::ostream&
+    operator<<(std::ostream& s, const size2f& r)
+    {
+        s << "(" << r.width << " x " << r.height << ")";
+        return s;
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    
+    size2i::size2i()
+        : width(-1)
+        , height(-1)
+    {
+    }
+
+    [[nodiscard]] size2i
+    size2i::create_from_width_height(int w, int h)
+    {
+        return size2i(w, h);
+    }
+
+    [[nodiscard]] size2i
+    size2i::create_from_height_width(int h, int w)
+    {
+        return size2i(w, h);
+    }
+
+    [[nodiscard]] size2i
+    size2i::create_from_square(int s)
+    {
+        return size2i(s, s);
+    }
+
+    void
+    size2i::operator+=(const size2i& rhs)
+    {
+        width += rhs.width;
+        height += rhs.height;
+    }
+
+    void
+    size2i::operator-=(const size2i& rhs)
+    {
+        width -= rhs.width;
+        height -= rhs.height;
+    }
+
+    void
+    size2i::operator*=(int rhs)
+    {
+        width *= rhs;
+        height *= rhs;
+    }
+
+    vec2<int>
+    size2i::calculate_center_offset_for(const size2i& o)
+    {
+        const int x = (width - o.width) / 2;
+        const int y = (height - o.height) / 2;
+        return vec2<int>(x, y);
+    }
+
+
+    size2i::size2i(int w, int h)
+        : width(w)
+        , height(h)
+    {
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+
+
+    std::string
+    CustomArgparser<size2i>::to_string(const size2i& s)
+    {
+        return "{}x{}"_format(s.width, s.height);
+    }
+
+    Result<size2i>
+    CustomArgparser<size2i>::parse(const std::string& value)
+    {
+        using R = Result<size2i>;
+        const auto values = split(value, 'x');
+        const auto xes = values.size();
+        if(xes != 2)
+        {
+            if(xes < 2)
+            {
+                return R::create_error
+                (
+                    "{} contains less than one x"_format(value)
+                );
+            }
+            else
+            {
+                return R::create_error
+                (
+                    "{} contains more than one x: {}"_format(value, xes-1)
+                );
+            }
+        }
+
+        auto parse_hs = [&](int index)
+        {
+            return argparse::default_parse_function<int>
+            (
+                trim(values[index])
+            );
+        };
+        const auto lhs = parse_hs(0);
+        const auto rhs = parse_hs(1);
+
+        if(!lhs) { return R::create_error(lhs.get_error()); }
+        if(!rhs) { return R::create_error(rhs.get_error()); }
+
+        return R::create_value(size2i::create_from_width_height(*lhs, *rhs));
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+
+    size2i
+    min(const size2i lhs, const size2i rhs)
+    {
+        return size2i::create_from_width_height
+        (
+            min(lhs.width, rhs.width), min(lhs.height, rhs.height)
+        );
+    }
+
+    size2i
+    max(const size2i lhs, const size2i rhs)
+    {
+        return size2i::create_from_width_height
+        (
+            max(lhs.width, rhs.width), max(lhs.height, rhs.height)
+        );
+    }
+
+    size2i
+    operator+(const size2i& lhs, const size2i& rhs)
+    {
+        size2i t = lhs;
+        t += rhs;
+        return t;
+    }
+
+    size2i
+    operator-(const size2i& lhs, const size2i& rhs)
+    {
+        size2i t = lhs;
+        t -= rhs;
+        return t;
+    }
+
+    size2i operator*(const size2i& lhs, int rhs)
+    {
+        size2i t = lhs;
+        t *= rhs;
+        return t;
+    }
+
+    size2i operator*(int lhs, const size2i& rhs)
+    {
+        size2i t = rhs;
+        t *= lhs;
+        return t;
+    }
+
+    std::ostream&
+    operator<<(std::ostream& s, const size2i& r)
+    {
+        s << "(" << r.width << " x " << r.height << ")";
+        return s;
+    }
+}
+
+
--- libs/core/src/core/size2.h
+++ libs/core/src/core/size2.h
@@ -8,206 +8,172 @@
 
 namespace euphoria::core
 {
-    template <typename T>
-    struct size2
+    struct size2f
     {
-        using Self = size2<T>;
+        float width;
+        float height;
 
-        T width;
-        T height;
+        size2f()
+        ;
 
-        size2()
-            : width(-1)
-            , height(-1)
-        {
-        }
+        [[nodiscard]] static size2f
+        create_from_width_height(float w, float h)
+        ;
 
-        [[nodiscard]]
-        static
-        Self
-        create_from_width_height(T w, T h)
-        {
-            return Self(w, h);
-        }
+        [[nodiscard]] static size2f
+        create_from_height_width(float h, float w)
+        ;
 
-        [[nodiscard]]
-        static
-        Self
-        create_from_height_width(T h, T w)
-        {
-            return Self(w, h);
-        }
-
-        [[nodiscard]]
-        static
-        Self
-        create_from_square(T s)
-        {
-            return Self(s, s);
-        }
+        [[nodiscard]] static size2f
+        create_from_square(float s)
+        ;
 
         void
-        operator+=(const Self& rhs)
-        {
-            width += rhs.width;
-            height += rhs.height;
-        }
+        operator+=(const size2f& rhs)
+        ;
 
         void
-        operator-=(const Self& rhs)
-        {
-            width -= rhs.width;
-            height -= rhs.height;
-        }
+        operator-=(const size2f& rhs)
+        ;
 
         void
-        operator*=(const T& rhs)
-        {
-            width *= rhs;
-            height *= rhs;
-        }
+        operator*=(float rhs)
+        ;
 
         void
-        operator/=(const T& rhs)
-        {
-            width /= rhs;
-            height /= rhs;
-        }
+        operator/=(float rhs)
+        ;
 
-        vec2<T>
-        calculate_center_offset_for(const Self& o)
-        {
-            const T x = (width - o.width) / 2;
-            const T y = (height - o.height) / 2;
-            return vec2<T>(x, y);
-        }
+        vec2<float>
+        calculate_center_offset_for(const size2f& o)
+        ;
 
     private:
-        size2(T w, T h) : width(w), height(h) {}
+        size2f(float w, float h);
     };
 
-
-    template<typename T>
-    struct CustomArgparser<size2<T>>
+    template<>
+    struct CustomArgparser<size2f>
     {
         enum { value = 1 };
 
-        static
-        std::string
-        to_string(const size2<T>& s)
-        {
-            return "{}x{}"_format(s.width, s.height);
-        }
+        static std::string
+        to_string(const size2f& s)
+        ;
 
-        static
-        Result<size2<T>>
+        static Result<size2f>
         parse(const std::string& value)
-        {
-            using R = Result<size2<T>>;
-            const auto values = split(value, 'x');
-            const auto xes = values.size();
-            if(xes != 2)
-            {
-                if(xes < 2)
-                {
-                    return R::create_error
-                    (
-                        "{} contains less than one x"_format(value)
-                    );
-                }
-                else
-                {
-                    return R::create_error
-                    (
-                        "{} contains more than one x: {}"_format(value, xes-1)
-                    );
-                }
-            }
-
-            auto parse_hs = [&](int index)
-            {
-                return argparse::default_parse_function<T>
-                (
-                    trim(values[index])
-                );
-            };
-            const auto lhs = parse_hs(0);
-            const auto rhs = parse_hs(1);
-
-            if(!lhs) { return R::create_error(lhs.get_error()); }
-            if(!rhs) { return R::create_error(rhs.get_error()); }
-
-            return R::create_value(size2<T>::create_from_width_height(*lhs, *rhs));
-        }
+        ;
     };
 
-    template <typename T>
-    size2<T>
-    min(const size2<T> lhs, const size2<T> rhs)
-    {
-        return size2<T>::create_from_width_height(
-                min(lhs.width, rhs.width), min(lhs.height, rhs.height));
-    }
+    size2f
+    min(const size2f lhs, const size2f rhs)
+    ;
 
-    template <typename T>
-    size2<T>
-    max(const size2<T> lhs, const size2<T> rhs)
-    {
-        return size2<T>::create_from_width_height(
-                max(lhs.width, rhs.width), max(lhs.height, rhs.height));
-    }
+    size2f
+    max(const size2f lhs, const size2f rhs)
+    ;
 
-    template <typename T>
-    size2<T>
-    operator+(const size2<T>& lhs, const size2<T>& rhs)
-    {
-        size2<T> t = lhs;
-        t += rhs;
-        return t;
-    }
+    size2f
+    operator+(const size2f& lhs, const size2f& rhs)
+    ;
 
-    template <typename T>
-    size2<T>
-    operator-(const size2<T>& lhs, const size2<T>& rhs)
-    {
-        size2<T> t = lhs;
-        t -= rhs;
-        return t;
-    }
+    size2f
+    operator-(const size2f& lhs, const size2f& rhs)
+    ;
 
-    template <typename T>
-    size2<T> operator*(const size2<T>& lhs, const T& rhs)
-    {
-        size2<T> t = lhs;
-        t *= rhs;
-        return t;
-    }
+    size2f operator*(const size2f& lhs, float rhs)
+    ;
 
-    template <typename T>
-    size2<T> operator*(const T& lhs, const size2<T>& rhs)
-    {
-        size2<T> t = rhs;
-        t *= lhs;
-        return t;
-    }
+    size2f operator*(float lhs, const size2f& rhs)
+    ;
 
-    template <typename T>
-    size2<T>
-    operator/(const size2<T>& lhs, const T& rhs)
-    {
-        size2<T> t = lhs;
-        t /= rhs;
-        return t;
-    }
+    size2f
+    operator/(const size2f& lhs, float rhs)
+    ;
 
-    template <typename S, typename T>
-    S&
-    operator<<(S& s, const size2<T>& r)
-    {
-        s << "(" << r.width << " x " << r.height << ")";
-        return s;
-    }
+    std::ostream&
+    operator<<(std::ostream& s, const size2f& r)
+    ;
 
-    using size2i = size2<int>;
-    using size2f = size2<float>;
+
+    struct size2i
+    {
+        int width;
+        int height;
+
+        size2i();
+
+        [[nodiscard]] static size2i
+        create_from_width_height(int w, int h)
+        ;
+
+        [[nodiscard]] static size2i
+        create_from_height_width(int h, int w)
+        ;
+
+        [[nodiscard]] static size2i
+        create_from_square(int s)
+        ;
+
+        void
+        operator+=(const size2i& rhs)
+        ;
+
+        void
+        operator-=(const size2i& rhs)
+        ;
+
+        void
+        operator*=(int rhs)
+        ;
+
+        vec2<int>
+        calculate_center_offset_for(const size2i& o)
+        ;
+
+    private:
+        size2i(int w, int h);
+    };
+
+
+    template<>
+    struct CustomArgparser<size2i>
+    {
+        enum { value = 1 };
+
+        static std::string
+        to_string(const size2i& s)
+        ;
+
+        static Result<size2i>
+        parse(const std::string& value)
+        ;
+    };
+
+    size2i
+    min(const size2i lhs, const size2i rhs)
+    ;
+
+    size2i
+    max(const size2i lhs, const size2i rhs)
+    ;
+
+    size2i
+    operator+(const size2i& lhs, const size2i& rhs)
+    ;
+
+    size2i
+    operator-(const size2i& lhs, const size2i& rhs)
+    ;
+
+    size2i operator*(const size2i& lhs, int rhs)
+    ;
+
+    size2i operator*(int lhs, const size2i& rhs)
+    ;
+
+    std::ostream&
+    operator<<(std::ostream& s, const size2i& r)
+    ;
 }
