f6ae0978500b3a059a623c25906d9d79dadf4bd2
madeso <madeso@users.noreply.github.com>
2023-03-26 20:48:14 +0000

    deploy: 640ef1ef3b23a760d4fac0c559d4727ca3fce888

--- doxygen-html/menudata.js
+++ doxygen-html/menudata.js
@@ -247,6 +247,7 @@ var menudata={children:[
 {text:"u",url:"globals.html#index_u"},
 {text:"v",url:"globals.html#index_v"},
 {text:"w",url:"globals.html#index_w"}]},
+{text:"Functions",url:"globals_func.html"},
 {text:"Variables",url:"globals_vars.html"},
 {text:"Typedefs",url:"globals_type.html"},
 {text:"Macros",url:"globals_defs.html",children:[
