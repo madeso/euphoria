8ba350364f77e3b9f132ef93f06e252314d64d9f
Gustav <sir.gustav.the.coder@gmail.com>
2024-12-28 10:43:02 +0100

    Document range

--- libs/base/src/base/range.h
+++ libs/base/src/base/range.h
@@ -20,6 +20,7 @@ namespace eu
         T lower_bound;
         T upper_bound;
 
+        /// Asserts if max is below min
         constexpr R(T min, T max) : lower_bound(min), upper_bound(max)
         {
             ASSERTX(lower_bound <= upper_bound, upper_bound, lower_bound);
@@ -33,23 +34,29 @@ namespace eu
     };
 
 
+    /// Create a range from `min` to `max` (inclusive)
     template <typename T>
     R<T> make_range(T min, T max)
     {
         return {min, max};
     }
 
+    /// Create a range from zero (0) to `max` (inclusive)
     template <typename T>
     R<T> make_range(T max)
     {
         return R<T>(0, max);
     }
 
+    /// A range going from 0 to 1
     constexpr R<float> r01 = { 0.0f, 1.0f};
+
+    // A range going from -1 to 1
     constexpr R<float> r11 = { -1.0f, 1.0};
 
     float from_01f(float lower_bound, float upper_bound, float value);
 
+    /// Converts a value in 0-1 range to a custom range
     template <typename T>
     T from_01(const R<T>& range, float value)
     {
@@ -71,6 +78,7 @@ namespace eu
     template <>
     float from_01(const R<float>& range, float value);
 
+    /// Converts a value in a range to the 0-1 range
     template <typename T>
     float to01(const R<T>& range, T value)
     {
@@ -88,20 +96,23 @@ namespace eu
                - half_difference * cos(An::from_percent_of_360(value));
     }
 
+    /// Remaps a value from one range to another
     template <typename T, typename F>
     T remap_to(const R<F>& from, const R<T>& to, F value)
     {
         return from_01(to, to01(from, value));
     }
 
-    // includsive, both min and max are included
+    /// Returns true if a value is withing a range
     template <typename T>
     bool is_within(const R<T>& range, T value)
     {
         return value >= range.lower_bound && value <= range.upper_bound;
     }
 
-    // inclusive, both min and max are included
+    /// Returns a value that is kept within the range.
+    /// i.e the value can't go lower than the lower bound or higher than the higher bound
+    /// @see wrap
     template <typename T>
     T keep_within(const R<T>& range, T value)
     {
@@ -117,6 +128,8 @@ namespace eu
         return value;
     }
 
+    /// Returns a value that wraps around the range.
+    /// When the value go outside the range, the value is wrapped back to either the start if exited at the end or vice versa.
     template <typename T>
     T wrap(const R<T>& range, T value)
     {
