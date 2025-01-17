e4ea58819f4e1cb80ab2c7d7c007f9d9e623d22b
Gustav <sir.gustav.the.coder@gmail.com>
2025-01-17 21:52:29 +0100

    Add guid docs

--- libs/base/src/base/guid.h
+++ libs/base/src/base/guid.h
@@ -8,11 +8,17 @@
 
 namespace eu
 {
+    /// Represents a universally unique identifier.
     struct Guid
     {
         std::array<u8, 16> data;
 
+
+        /// Does nothing.
+        /// @see \ref eu::nil_guid
         Guid() = default;
+
+        /// Set the guid data.
         constexpr explicit Guid(std::array<u8, 16> d)
             : data(std::move(d))
         {
@@ -21,9 +27,13 @@ namespace eu
         [[nodiscard]] static std::optional<Guid> generate();
     };
 
+    /// Parse a guid from a string
     [[nodiscard]] std::optional<Guid> guid_from(const std::string& str);
+
+    /// Convert a guid to a string representation, prefer fmt.
     [[nodiscard]] std::string string_from(const Guid& g);
 
+    /// A nil guid, all bits are set to 0.
     constexpr Guid nil_guid = Guid
     {
         std::array<u8, 16>
@@ -36,3 +46,5 @@ namespace eu
         }
     };
 }
+
+ADD_DEFAULT_FORMATTER(eu::Guid, std::string, eu::string_from);
