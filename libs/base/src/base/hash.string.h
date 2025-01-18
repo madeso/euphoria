86bb8e2a2144ebb715b84950381d0da8b1dc77b2
Gustav <sir.gustav.the.coder@gmail.com>
2025-01-18 21:36:47 +0100

    Add documentation for Hsh

--- libs/base/src/base/hash.string.h
+++ libs/base/src/base/hash.string.h
@@ -17,13 +17,20 @@ namespace eu
 constexpr u64 hash64(const std::string_view str, u64 hash = 0xcbf29ce484222325)
     { return str.empty() ? hash : hash64(str.substr(1), (hash ^ str[0]) * 0x100000001b3); }
 
+/// Hash of a non-owning string.
+/// It will be (pretty) printed as the original string if that is enabled.
+/// Treat it as a regular `std::string_view` but without the string functionality.
+/// @see \ref hash64
 struct Hsh
 {
+    /// The actual hash. Prefer to use operators on \ref Hsh instead of accessing this directly.
     const u64 hash;
+
     #if USE_HASH_TEXT == 1
     const std::string_view text;
     #endif
 
+    /// Creates a new hash object and computes the hash at compile time if possible.
     constexpr Hsh(const std::string_view s)
         : hash( hash64(s) )
         #if USE_HASH_TEXT == 1
