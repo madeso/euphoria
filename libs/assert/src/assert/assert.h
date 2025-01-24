2c7bb64917b8e1f0cd6c591f64fa03e4e41321d4
Gustav <sir.gustav.the.coder@gmail.com>
2025-01-24 21:09:59 +0100

    Add documentation for assert

--- libs/assert/src/assert/assert.h
+++ libs/assert/src/assert/assert.h
@@ -3,15 +3,13 @@
 #include <string>
 #include <vector>
 
-#ifdef _MSC_VER
-#define __PRETTY_FUNCTION__ __FUNCSIG__
-#define BREAK_IN_DEBUG() __debugbreak()
-#else
+/// implements a break in debug.
+/// \hideinitializer
 #define BREAK_IN_DEBUG() \
     do \
     { \
     } while(false)
-#endif
+
 
 #ifdef RELEASE
 
@@ -23,9 +21,14 @@
 
 #else
 
+/// Defines that the assert support library should be implemented.
+/// @todo Expose as as cmake variable instead of looking at the build flag.
 #define IMPLEMENT_ASSERT_LIB
 
 // todo(Gustav): stb libraries and rapidjson aren't using our assert
+/// Assert that a value is true.
+/// @param x the value to check.
+/// \hideinitializer
 #define ASSERT(x) \
     do \
     { \
@@ -45,6 +48,10 @@
         } \
     } while(false)
 
+/// Assert that a value is true.
+/// @param x the value to check.
+/// @param ... any extra value to attach to the assert message
+/// \hideinitializer
 #define ASSERTX(x, ...) \
     do \
     { \
@@ -64,6 +71,9 @@
         } \
     } while(false)
 
+/// Assert that something bad has happened.
+/// @param message the reason to fail.
+/// \hideinitializer
 #define DIE(message) \
     ::eu::assertlib::on_assert( \
             message, \
@@ -73,6 +83,10 @@
             {}, \
             __PRETTY_FUNCTION__)
 
+/// Assert that something bad has happened.
+/// @param message the reason to fail.
+/// @param ... any extra value to attach to the assert message
+/// \hideinitializer
 #define DIEX(message, ...) \
     ::eu::assertlib::on_assert( \
             message, \
@@ -86,8 +100,10 @@
 
 #ifdef IMPLEMENT_ASSERT_LIB
 
+/// \private
 namespace eu::assertlib
 {
+    /// \private
     struct AssertArgumentValue
     {
         std::string value;
@@ -98,12 +114,15 @@ namespace eu::assertlib
             {}
     };
 
+    /// \private
     void
     begin_throwing();
 
+    /// \private
     bool
     is_throwing();
 
+    /// \private
     void
     on_assert
     (
