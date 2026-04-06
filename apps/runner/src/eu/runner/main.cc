d16ca6a813837cb2e1dce510741a53792b9c55e4
Gustav <sir.gustav.the.coder@gmail.com>
2026-04-06 14:59:34 +0200

    Add runner project

---
+++ apps/runner/src/eu/runner/main.cc
@@ -0,0 +1,142 @@
+#include "SDL.h"
+
+#include <cassert>
+#include <string>
+
+#include "eu/log/log.h"
+#include "eu/base/memorychunk.h"
+
+#include "eu/render/canvas.h"
+#include "eu/render/state.h"
+#include "eu/render/font.h"
+#include "eu/render/opengl_utils.h"
+#include "eu/render/texture.io.h"
+
+#include "eu/render/enable_high_performance_graphics.h"
+
+
+ENABLE_HIGH_PERFORMANCE_GRAPHICS
+
+
+int main(int, char**)
+{
+    int window_width = 1280;
+    int window_height = 720;
+    const char* glsl_version = "#version 130";
+
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_EVENTS) < 0) {
+        LOG_ERR("Error initializing SDL: {}", SDL_GetError());
+        return -1;
+    }
+
+#if defined(__APPLE__)
+    // GL 3.2 Core + GLSL 150
+    const char* glsl_version = "#version 150";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);	// Always required on Mac
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+#else
+    // GL 3.0 + GLSL 130
+    // const char* glsl_version = "#version 130";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);  // was 0 in dear imgui example??
+#endif
+
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+
+    SDL_Window* window = SDL_CreateWindow("Runner", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        window_width, window_height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
+    if (!window) {
+        LOG_ERR("Error creating window: {}", SDL_GetError());
+        return -1;
+    }
+    
+    auto* glContext = SDL_GL_CreateContext(window);
+    SDL_GetWindowSize(window, &window_width, &window_height);
+    if (!glContext) {
+        LOG_ERR("Error creating gl context: {}", SDL_GetError());
+        return -1;
+    }
+
+    /* OpenGL setup */
+    const int glad_result = gladLoadGLLoader(SDL_GL_GetProcAddress);
+    if (glad_result == 0)
+    {
+        LOG_ERR("Failed to init glad, error: {0}", glad_result);
+        return -1;
+    }
+
+    {
+        const std::string gl_vendor = reinterpret_cast<const char*>(glGetString(GL_VENDOR));
+        const std::string gl_renderer = reinterpret_cast<const char*>(glGetString(GL_RENDERER));
+        const std::string gl_version = reinterpret_cast<const char*>(glGetString(GL_VERSION));
+        const std::string gl_shading_language_version = reinterpret_cast<const char*>(glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+        LOG_INFO("Vendor:         {0}", gl_vendor);
+        LOG_INFO("Renderer:       {0}", gl_renderer);
+        LOG_INFO("Version OpenGL: {0}", gl_version);
+        LOG_INFO("Version GLSL:   {0}", gl_shading_language_version);
+    }
+
+    eu::render::State states;
+    eu::render::Render2 render{ &states };
+
+    bool running = true;
+    
+    LOG_INFO("Runner started");
+    while (running)
+    {
+        SDL_Event e;
+        while (SDL_PollEvent(&e) != 0)
+        {
+            switch (e.type)
+            {
+            case SDL_WINDOWEVENT:
+                if (e.window.event == SDL_WINDOWEVENT_RESIZED || e.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
+                {
+                    LOG_INFO("Resized");
+                    SDL_GetWindowSize(window, &window_width, &window_height);
+                }
+                break;
+            case SDL_QUIT: running = false; break;
+            default:
+                // ignore other events
+                break;
+            }
+        }
+
+        {
+            eu::render::RenderCommand cmd {.states = &states, .render = &render, .size = {.width = window_width, .height = window_height} };
+
+            // todo(Gustav): provide a pixel layout
+            const auto screen = eu::render::LayoutData{ .style = eu::render::ViewportStyle::extended,
+                                                     .requested_width = static_cast<float>(window_width), .requested_height = static_cast<float>(window_height) };
+
+            cmd.clear(eu::colors::blue_sky, screen);
+
+            auto layer = eu::render::with_layer2(cmd, screen);
+            eu::render::Quad{ .tint = eu::colors::green_bluish }.draw(layer.batch, layer.viewport_aabb_in_worldspace.get_bottom(50));
+        }
+
+        SDL_GL_SwapWindow(window);
+    }
+
+    LOG_INFO("Shutting down");
+    SDL_GL_DeleteContext(glContext);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
