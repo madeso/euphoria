872bd10d41963c511bb141e4ab7efd5655d52e90
Gustav <sir.gustav.the.coder@gmail.com>
2026-04-19 10:08:54 +0200

    Add mesh concept

---
+++ libs/core/src/eu/core/geom.cc
@@ -0,0 +1,2 @@
+#include "eu/core/geom.h"
+
---
+++ libs/core/src/eu/core/mesh.h
@@ -0,0 +1,29 @@
+#pragma one
+
+#include "eu/core/geom.h"
+
+
+namespace eu::core
+{
+
+struct MeshGeom
+{
+    Geom geom;
+    // std::size_t material;
+};
+
+struct TransformedMesh
+{
+    std::string name;
+    m4 transform = m4_identity;
+    std::vector<MeshGeom> geoms;
+};
+
+struct Mesh
+{
+    // todo(Gustav): change the single list to a hierachy?
+    std::vector<TransformedMesh> meshes;
+    // std::vector<Material> materials;
+};
+
+}
